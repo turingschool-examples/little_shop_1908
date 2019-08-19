bd1e632cc41b8be043661dfa9a50775baa173eab
Tyler Schaffer <tpschaffer@gmail.com>
2019-08-19 14:37:53 -0600

    Test reviews showing on the item show page
    Co-authored-by: Evette Telyas <50186721+evettetelyas@users.noreply.github.com>

--- spec/features/items/show_spec.rb
+++ spec/features/items/show_spec.rb
@@ -16,4 +16,22 @@ RSpec.describe 'item show page', type: :feature do
     expect(page).to have_content("Sold by: #{bike_shop.name}")
     expect(page).to have_css("img[src*='#{chain.image}']")
   end
+
+  it 'shows reviews for that item' do
+    dog_shop = Merchant.create(name: "Brian's Dog Shop", address: '125 Doggo St.', city: 'Denver', state: 'CO', zip: 80210)
+    pull_toy = dog_shop.items.create(name: "Pull Toy", description: "Great pull toy!", price: 10, image: "http://lovencaretoys.com/image/cache/dog/tug-toy-dog-pull-9010_2-800x800.jpg", inventory: 32)
+    review_1 = pull_toy.reviews.create(title: "This toy rules", content: "I bought this for my dog and it rules", rating: 5)
+    review_2 = pull_toy.reviews.create(title: "This toy sucks", content: "My dog hates this toy", rating: 1)
+
+    visit "items/#{pull_toy.id}"
+
+    within "#review-#{pull_toy.id}"
+      expect(page).to have_content(review_1.title)
+      expect(page).to have_content(review_1.content)
+      expect(page).to have_content(review_1.rating)
+      expect(page).to have_content(review_2.title)
+      expect(page).to have_content(review_2.content)
+      expect(page).to have_content(review_2.rating)
+      
+  end
 end
