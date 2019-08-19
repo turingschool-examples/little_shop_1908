360878007843c77d21fb842ea565c5c5120c6c2a
Leiya Kenney <leiya.kenney@gmail.com>
2019-08-19 14:44:38 -0600

    Attempts to redownload Gemfile.lock for Heroku

--- spec/features/items/show_spec.rb
+++ spec/features/items/show_spec.rb
@@ -22,8 +22,19 @@ RSpec.describe 'item show page', type: :feature do
     chain = bike_shop.items.create(name: "Chain", description: "It'll never break!", price: 50, image: "https://www.rei.com/media/b61d1379-ec0e-4760-9247-57ef971af0ad?size=784x588", inventory: 5)
     review_1 = chain.reviews.create(title: "Great", content: "I like this chain!", rating: 4)
     review_2 = chain.reviews.create(title: "Ew", content: "The worst", rating: 1)
+    # reviews = [chain.reviews]
 
     visit "items/#{chain.id}"
+
+    # within '.reviews-section' do 
+    #   reviews.each do |review|
+    #     within "#review-#{review.id}" do
+    #       expect(page).to have_content(review.title)
+    #       expect(page).to have_content(review.content)
+    #       expect(page).to have_content("Rating: #{review.title}")
+    #     end
+    #   end
+    # end
 
     within "#review-#{review_1.id}" do
       expect(page).to have_content(review_1.title)
