865c6da5afa9aa8ad68dc22f6fb7b35b86cea980
Joshua Sherwood <jkjgsherwood@gmail.com>
2019-08-20 15:25:16 -0600

    Add catch_404 method and rescue_from method
    Co-authored-by: Nancy Lee <24424825+nancylee713@users.noreply.github.com>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,18 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   helper_method :cart
+
+  rescue_from ActionController::RoutingError do |exception|
+    logger.error 'Routing error occurred'
+    redirect_to controller: 'home', action: 'index'
+    flash[:error] = "The page you have selected does not exist"
+  end
+
   def cart
     @cart ||= Cart.new(session[:cart])
+  end
+
+  def catch_404
+    raise ActionController::RoutingError.new(params[:path])
   end
 end
