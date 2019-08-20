4e02728eabec90ba86c621155de19a2a458f1f80
Joshua Sherwood <jkjgsherwood@gmail.com>
2019-08-20 15:25:16 -0600

    Add create and show methods in order controller
    Co-authored-by: Nancy Lee <24424825+nancylee713@users.noreply.github.com>

--- app/controllers/order_controller.rb
+++ app/controllers/order_controller.rb
@@ -2,4 +2,20 @@ class OrderController <ApplicationController
   def new
     @cart = Cart.new(session[:cart])
   end
+
+  def create
+    @order = Order.create(order_params)
+    redirect_to "/orders/#{@order.id}"
+  end
+
+  def show
+    @order = Order.find(params[:id])
+    @cart = Cart.new(session[:cart])
+  end
+
+  private
+
+  def order_params
+    params.permit(:name,:address,:city,:state,:zip)
+  end
 end
