07f14291ef2a8f98d4d02ab05a559280897cf91b
Tyler Schaffer <tpschaffer@gmail.com>
2019-08-20 15:18:06 -0600

    Fix edit review test

--- spec/features/reviews/edit_spec.rb
+++ spec/features/reviews/edit_spec.rb
@@ -9,20 +9,19 @@ describe "Review Edit Page" do
     @review_1 = @pull_toy.reviews.create(title: "This toy rules", content: "I bought this for my dog and it rules", rating: 5)
   end
 
-  xit 'has a form to update a review with prepopulated info' do
+  it 'has a form to update a review with prepopulated info' do
     visit "/items/#{@pull_toy.id}/reviews/#{@review_1.id}/edit-review"
 
     expect(find_field('Title').value).to eq(@review_1.title)
     expect(find_field('Content').value).to eq(@review_1.content)
-    expect(find_field('Rating').value).to eq(@review_1.rating)
+    expect(find_field('Rating').value.to_i).to eq(@review_1.rating)
 
     title = "It broke! :("
     content = "My dog loved this until it broke."
 
     fill_in 'Title', with: title
     fill_in 'Content', with: content
-    find("#rating").click
-    find("#rating option", :text => '3').click
+    select '3', from: :rating
 
     click_button "Submit Updated Review"
 
@@ -31,6 +30,6 @@ describe "Review Edit Page" do
     expect(page).to_not have_content("This toy rules")
     expect(page).to have_content(content)
     expect(page).to_not have_content("I bought this for my dog and it rules")
-    expect(page).to have_content("3")
+    expect(page).to have_content("3/5")
   end
 end
