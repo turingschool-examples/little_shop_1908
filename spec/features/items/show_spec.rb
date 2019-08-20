a583cfb935033ee016b854f234efda5675fd982e
Joshua Sherwood <jkjgsherwood@gmail.com>
2019-08-20 15:25:16 -0600

    Add spec tests for editing a review and showing flash messages when successful and when form is incomplete

--- spec/features/items/show_spec.rb
+++ spec/features/items/show_spec.rb
@@ -13,7 +13,11 @@ RSpec.describe 'item show page', type: :feature do
     @inactive_items = [@dog_bone]
     @bike_shop = Merchant.create(name: "Brian's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
     @chain = @bike_shop.items.create(name: "Chain", description: "It'll never break!", price: 50, image: "https://www.rei.com/media/b61d1379-ec0e-4760-9247-57ef971af0ad?size=784x588", inventory: 5)
+
     @good_review = @chain.reviews.create(title: "I like this product", content: "This is a great product. I will buy it again soon.", rating: 5)
+    @average_review = @chain.reviews.create(title: "So-so product", content: "This is okay.", rating: 3)
+    @negative_review = @chain.reviews.create(title: "I don't like this product", content: "This is not a great product. I will not buy it again soon.", rating: 2)
+    @terrible_review = @chain.reviews.create(title: "I hate it", content: "Never buy it again.", rating: 1)
   end
 
   it 'shows item info' do
@@ -59,8 +63,6 @@ RSpec.describe 'item show page', type: :feature do
 
     expect(current_path).to eq("/items/#{@chain.id}")
     expect(page).to have_content("You have successfully posted a review")
-    save_and_open_page
-
 
     within "#review-#{Review.last.id}" do
       expect(page).to have_content(title)
@@ -69,7 +71,36 @@ RSpec.describe 'item show page', type: :feature do
     end
   end
 
-  it 'shows alert flash messages when form is not completely filled' do
+  it 'can edit review for item and post flash message when review is edited' do
+    visit "/items/#{@chain.id}"
+
+    within "#review-#{@average_review.id}" do
+      click_link "edit review"
+    end
+
+    expect(current_path).to eq("/items/#{@chain.id}/#{@average_review.id}/edit")
+
+    title_2 = "This a better than average product"
+    content_2 = "This product was just above average"
+    rating_2 = 4
+
+    fill_in :title, with: title_2
+    fill_in :content, with: content_2
+    fill_in :rating, with: rating_2
+
+    click_button "Post Review"
+
+    expect(current_path).to eq("/items/#{@chain.id}")
+    expect(page).to have_content("You have successfully edited a review")
+
+    within "#review-#{@average_review.id}" do
+      expect(page).to have_content(title_2)
+      expect(page).to have_content(content_2)
+      expect(page).to have_content("Rating: #{rating_2}")
+    end
+  end
+
+  it 'shows alert flash messages when new review form is not completely filled' do
     visit "/items/#{@chain.id}"
     click_link "add new review"
 
@@ -83,6 +114,50 @@ RSpec.describe 'item show page', type: :feature do
 
     expect(current_path).to eq("/items/#{@chain.id}/review/new")
     expect(page).to have_content("You have not completed the form. Please complete all three sections to post a review.")
-    save_and_open_page
+  end
+
+  it 'shows alert flash message when edit review form is not completely filled' do
+    visit "/items/#{@chain.id}"
+
+    within "#review-#{@average_review.id}" do
+      click_link "edit review"
+    end
+
+    expect(current_path).to eq("/items/#{@chain.id}/#{@average_review.id}/edit")
+
+    title_2 = nil
+    fill_in :title, with: title_2
+
+    click_button "Post Review"
+
+    expect(current_path).to eq("/items/#{@chain.id}/#{@average_review.id}/edit")
+    expect(page).to have_content("You have not completed the form. Please complete all three sections to post a review.")
+  end
+
+  it 'shows review stats' do
+    visit "/items/#{@chain.id}"
+
+    within "#item-#{@chain.id}-review-stats" do
+      expect(page).to have_content("Average Rating: #{@chain.reviews.average(:rating)}")
+    end
+
+    within "#item-#{@chain.id}-top-3-reviews" do
+      top_three_title_rating = @chain.reviews.order(:rating).reverse[0..2].pluck(:title, :rating)
+
+      expect(page).to have_content("#{top_three_title_rating[0][0]}")
+      expect(page).to have_content("#{top_three_title_rating[0][1]}")
+      expect(page).to have_content("#{top_three_title_rating[1][0]}")
+      expect(page).to have_content("#{top_three_title_rating[1][1]}")
+    end
+
+    within "#item-#{@chain.id}-bottom-3-reviews" do
+      bottom_three_title_rating = @chain.reviews.order(:rating)[0..2].pluck(:title, :rating)
+
+      expect(page).to have_content("#{bottom_three_title_rating[0][0]}")
+      expect(page).to have_content("#{bottom_three_title_rating[0][1]}")
+      expect(page).to have_content("#{bottom_three_title_rating[1][0]}")
+      expect(page).to have_content("#{bottom_three_title_rating[1][1]}")
+    end
+
   end
 end
