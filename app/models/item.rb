ea227205720eb6c95833d4b50a1865215cd45a12
Joshua Sherwood <jkjgsherwood@gmail.com>
2019-08-20 15:25:16 -0600

    Add dependent destroy rule on reviews in item model
    Co-authored-by: Nancy Lee <24424825+nancylee713@users.noreply.github.com>

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,6 @@
 class Item <ApplicationRecord
   belongs_to :merchant
-  has_many :reviews, dependent: :destroy
+  has_many :reviews, :dependent => :destroy
   has_many :items_orders
   has_many :orders, through: :items_orders
 
