c801f71de50889b2b42278d18d1d4479696636b2
Tyler Schaffer <tpschaffer@gmail.com>
2019-08-19 20:52:52 -0600

    Update merchant index and show tests

--- spec/features/merchants/show_spec.rb
+++ spec/features/merchants/show_spec.rb
@@ -1,27 +1,40 @@
 require 'rails_helper'
 
-RSpec.describe 'merchant show page', type: :feature do
-  describe 'As a user' do
-    before :each do
-      @bike_shop = Merchant.create(name: "Brian's Bike Shop", address: '123 Bike Rd.', city: 'Richmond', state: 'VA', zip: 23137)
-    end
-
-    it 'I can see a merchants name, address, city, state, zip' do
-      visit "/merchants/#{@bike_shop.id}"
-
-      expect(page).to have_content("Brian's Bike Shop")
-      expect(page).to have_content("123 Bike Rd.\nRichmond, VA 23137")
-    end
-
-    it 'I can see a link to visit the merchant items' do
-      visit "/merchants/#{@bike_shop.id}"
-
-      expect(page).to have_link("All #{@bike_shop.name} Items")
-
-      click_on "All #{@bike_shop.name} Items"
-
-      expect(current_path).to eq("/merchants/#{@bike_shop.id}/items")
-    end
-
+describe 'Merchant Show Page' do
+  before(:each) do
+    @bike_shop = Merchant.create(name: "Brian's Bike Shop", address: '123 Bike Rd.', city: 'Richmond', state: 'VA', zip: 23137)
   end
+
+  xit "displays a merchant's name, address, city, state, zip" do
+    visit "/merchants/#{@bike_shop.id}"
+    expect(page).to have_content(@bike_shop.name)
+    expect(page).to have_content("#{@bike_shop.address}\n#{@bike_shop.city}, #{@bike_shop.state} #{@bike_shop.zip}")
+  end
+
+  xit 'has a link to visit the merchant items' do
+    visit "/merchants/#{@bike_shop.id}"
+    expect(page).to have_link("All #{@bike_shop.name} Items")
+    click_link "All #{@bike_shop.name} Items"
+    expect(current_path).to eq("/merchants/#{@bike_shop.id}/items")
+  end
+
+  describe 'has a link to delete the merchant' do
+    xit 'if the merchant has no items' do
+      visit "/merchants/#{@bike_shop.id}"
+      expect(page).to have_link("Delete Merchant")
+      click_on "Delete Merchant"
+      expect(current_path).to eq('/merchants')
+      expect(page).to_not have_content(@bike_shop.name)
+    end
+
+    xit 'if the merchant has items and no orders' do
+      chain = @bike_shop.items.create(name: "Chain", description: "It'll never break!", price: 50, image: "https://www.rei.com/media/b61d1379-ec0e-4760-9247-57ef971af0ad?size=784x588", inventory: 5)
+
+      visit "/merchants/#{@bike_shop.id}"
+      expect(page).to have_link("Delete Merchant")
+      click_on "Delete Merchant"
+      expect(current_path).to eq('/merchants')
+      expect(page).to_not have_content(@bike_shop.name)
+    end
+  end 
 end
