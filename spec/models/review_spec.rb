8757fe4a98243bb10cd8d7104dc1016c9f27cfad
Nancy Lee <srlee713@gmail.com>
2019-08-20 16:48:07 -0600

    Add top_three_reviews class method in Review model
    Co-authored-by: Josh Sherwood <49769068+joshsherwood1@users.noreply.github.com>

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -12,13 +12,21 @@ describe Review, type: :model do
   end
 
   describe "class methods" do
-    it "average review rating" do
-      bike_shop = Merchant.create(name: "Brian's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
-      chain = bike_shop.items.create(name: "Chain", description: "It'll never break!", price: 50, image: "https://www.rei.com/media/b61d1379-ec0e-4760-9247-57ef971af0ad?size=784x588", inventory: 5)
-      good_review = chain.reviews.create(title: "I like this product", content: "This is a great product. I will buy it again soon.", rating: 5)
-      negative_review = chain.reviews.create(title: "I don't like this product", content: "This is not a great product. I will not buy it again soon.", rating: 2)
+    before(:each) do
+      @bike_shop = Merchant.create(name: "Brian's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
+      @chain = @bike_shop.items.create(name: "Chain", description: "It'll never break!", price: 50, image: "https://www.rei.com/media/b61d1379-ec0e-4760-9247-57ef971af0ad?size=784x588", inventory: 5)
+      @good_review = @chain.reviews.create(title: "I like this product", content: "This is a great product. I will buy it again soon.", rating: 5)
+      @average_review = @chain.reviews.create(title: "So-so product", content: "This is okay.", rating: 3)
+      @negative_review = @chain.reviews.create(title: "I don't like this product", content: "This is not a great product. I will not buy it again soon.", rating: 2)
+      @terrible_review = @chain.reviews.create(title: "I hate it", content: "Never buy it again.", rating: 1)
+    end
 
-      expect(Review.average_review_rating).to eq(3.5)
+    it "average review rating" do
+      expect(Review.average_review_rating).to eq(2.75)
+    end
+
+    it "shows top three ratings" do
+      expect(Review.top_three_reviews.length).to eq(3)
     end
   end
 end
