8fcabcdfd133c26b43ce59729340b7df756da1f8
Joshua Sherwood <jkjgsherwood@gmail.com>
2019-08-20 15:25:16 -0600

    Add cart add item feature spec test that verifies that flash message shows when user tries to add item to cart that is inactive
    Co-authored-by: Nancy Lee <24424825+nancylee713@users.noreply.github.com>

--- spec/features/cart/add_item_spec.rb
+++ spec/features/cart/add_item_spec.rb
@@ -4,8 +4,10 @@ RSpec.describe "As a Visitor" do
   before(:each) do
     @meg = Merchant.create(name: "Meg's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
     @tire = @meg.items.create(name: "Gatorskins", description: "They'll never pop!", price: 100, image: "https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588", inventory: 12)
+    @dog_bone = @meg.items.create(name: "Dog Bone", description: "They'll love it!", price: 21, image: "https://img.chewy.com/is/image/catalog/54226_MAIN._AC_SL1500_V1534449573_.jpg", active?:false, inventory: 21)
   end
-  it "When I add an item to a cart" do
+
+  it "I can add an item to a cart" do
 
     visit "/items/#{@tire.id}"
 
@@ -16,5 +18,20 @@ RSpec.describe "As a Visitor" do
     end
 
     expect(page).to have_content("You now have 1 copy of #{@tire.name} in your cart.")
+  end
+
+  it "I cannot add an inactive item to a cart" do
+
+    visit "/items/#{@dog_bone.id}"
+
+    expect(page).to have_button("Add to Cart")
+
+    within "#item-info" do
+      click_button "Add to Cart"
+    end
+
+    expect(current_path).to eq("/items/#{@dog_bone.id}")
+
+    expect(page).to have_content("You cannot add #{@dog_bone.name} to your cart because it is inactive.")
   end
 end
