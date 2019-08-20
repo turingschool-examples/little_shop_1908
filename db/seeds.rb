fc0e55b4c77dee7bac9e03efe861d48aa9dfb307
Corina Allen <StarPerfect@gmail.com>
2019-08-20 14:33:49 -0600

    Update Heroku Seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,7 +15,10 @@ dog_shop = Merchant.create(name: "Brian's Dog Shop", address: '125 Doggo St.', c
 
 #bike_shop items
 tire = bike_shop.items.create(name: "Gatorskins", description: "They'll never pop!", price: 100, image: "https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588", inventory: 12)
+tire_review = tire.reviews.create(title: 'Never Buy This Tire', content: "I bought two of these and they blew within a week of each other, a month after purchase", rating: 1)
 
 #dog_shop items
 pull_toy = dog_shop.items.create(name: "Pull Toy", description: "Great pull toy!", price: 10, image: "http://lovencaretoys.com/image/cache/dog/tug-toy-dog-pull-9010_2-800x800.jpg", inventory: 32)
+pull_toy_review = pull_toy.reviews.create(title: "Sparky love this!", content: "Sparky took it right off the shelf and began playing with it immediately so I bought him 2!", rating: 5)
 dog_bone = dog_shop.items.create(name: "Dog Bone", description: "They'll love it!", price: 21, image: "https://img.chewy.com/is/image/catalog/54226_MAIN._AC_SL1500_V1534449573_.jpg", active?:false, inventory: 21)
+dog_bone_review = dog_shop.reviews.create(title: 'What a dog bone!', content: "Taste great!", rating: 5)
