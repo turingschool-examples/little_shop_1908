60e84ed5746a01f1ac40373dc990204812c65afd
Joshua Sherwood <jkjgsherwood@gmail.com>
2019-08-20 15:25:16 -0600

    Create edit and update methods for review controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class ReviewsController <ApplicationController
 
   def new
@@ -14,6 +15,24 @@ class ReviewsController <ApplicationController
     else
       flash[:alert] = "You have not completed the form. Please complete all three sections to post a review."
       redirect_to "/items/#{@item.id}/review/new"
+    end
+  end
+
+  def edit
+    @review = Review.find(params[:review_id])
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    item = Item.find(params[:id])
+    review = Review.find(params[:review_id])
+    review.update(review_params)
+    if review.save
+      flash[:success] = "You have successfully edited a review"
+      redirect_to "/items/#{item.id}"
+    else
+      flash[:alert] = "You have not completed the form. Please complete all three sections to post a review."
+      redirect_to "/items/#{item.id}/#{review.id}/edit"
     end
   end
 
