0d7b1e0eafdaa5447e40dccf9dc53b1b41b1fb97
Tyler Schaffer <tpschaffer@gmail.com>
2019-08-19 22:39:11 -0600

    Create edit_spec tests for reviews

---
+++ spec/features/reviews/edit_spec.rb
@@ -0,0 +1,36 @@
+require 'rails_helper'
+
+describe "Review Edit Page" do
+  before(:each) do
+    @dog_shop = Merchant.create(name: "Brian's Dog Shop", address: '125 Doggo St.', city: 'Denver', state: 'CO', zip: 80210)
+
+    @pull_toy = @dog_shop.items.create(name: "Pull Toy", description: "Great pull toy!", price: 10, image: "http://lovencaretoys.com/image/cache/dog/tug-toy-dog-pull-9010_2-800x800.jpg", inventory: 32)
+
+    @review_1 = @pull_toy.reviews.create(title: "This toy rules", content: "I bought this for my dog and it rules", rating: 5)
+  end
+
+  xit 'has a form to update a review with prepopulated info' do
+    visit "/items/#{@pull_toy.id}/reviews/#{@review_1.id}/edit-review"
+
+    expect(find_field('Title').value).to eq(@review_1.title)
+    expect(find_field('Description').value).to eq(@review_1.description)
+    expect(find_field('Rating').value).to eq(@review_1.rating)
+
+    title = "It broke! :("
+    description = "My dog loved this until it broke."
+    rating = 3
+
+    fill_in 'Title', with: title
+    fill_in 'Description', with: description
+    fill_in 'Rating', with: rating
+
+    click_button "Submit Updated Review"
+
+    expect(current_path).to eq("/items/#{@pull_toy.id}")
+    expect(page).to have_content(title)
+    expect(page).to_not have_content("This toy rules")
+    expect(page).to have_content(description)
+    expect(page).to_not have_content("I bought this for my dog and it rules")
+    expect(page).to have_content("3")
+  end
+end
