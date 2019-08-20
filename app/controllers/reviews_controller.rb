6446650656d2a017366f2009205aa4061e35d14c
Fenton Taylor <fentontaylor@gmail.com>
2019-08-20 14:04:18 -0600

    Update routes, controller, and view page to handle destroy funtion
    Co-authored-by: Alec Wells <23268508+alect47@users.noreply.github.com>

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,28 +1,33 @@
 class ReviewsController<ApplicationController
-  before_action :set_review, only: [:index, :new, :create, :edit, :update, :destroy]
+  before_action :set_review, only: [:index, :edit, :update, :destroy]
   def index
+    @item = Item.find(params[:item_id])
     @reviews = @item.reviews
   end
 
   def new
+    @item = Item.find(params[:item_id])
   end
 
   def create
-    @item.reviews.create(review_params)
-    redirect_to "/items/#{@item.id}"
+    item = Item.find(params[:item_id])
+    item.reviews.create(review_params)
+    redirect_to "/items/#{item.id}"
   end
 
   def edit
+    @review = Review.find(params[:id])
     @item = @review.item
   end
 
   def update
+    review = Review.find(params[:id])
     @review.update(review_params)
     redirect_to "/items/#{@review.item_id}"
   end
 
   def destroy
-
+    redirect_to "/items/#{@review.item_id}"
   end
 
   private
