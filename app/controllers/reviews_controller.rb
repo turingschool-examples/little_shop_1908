4b8b9b103c8946c160d31862be9657c94ea04cd6
Nancy Lee <srlee713@gmail.com>
2019-08-21 00:11:52 -0600

    Create reviews controller
    Co-authored-by: Josh Sherwood <49769068+joshsherwood1@users.noreply.github.com>

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,20 +1,37 @@
 class ReviewsController <ApplicationController
+  def index
+    if params[:item_id]
+      @item = Item.find(params[:item_id])
+      @reviews = @item.reviews
+    else
+      @reviews = Review.all
+    end
+  end
 
   def new
-    #binding.pry
-    @item = Item.find(params[:id])
+    @item = Item.find(params[:item_id])
+  end
+
+  def show
+    @review = Review.find(params[:id])
   end
 
   def create
-    @item = Item.find(params[:id])
+    @item = Item.find(params[:item_id])
     review = @item.reviews.create(review_params)
     if review.save
       flash[:success] = "You have successfully posted a review"
       redirect_to "/items/#{@item.id}"
     else
       flash[:alert] = "You have not completed the form. Please complete all three sections to post a review."
-      redirect_to "/items/#{@item.id}/review/new"
+      redirect_to "/items/#{@item.id}/reviews/new"
     end
+  end
+
+  def destroy
+    review = Review.find(params[:id])
+    review.destroy
+    redirect_to "/reviews"
   end
 
   private
