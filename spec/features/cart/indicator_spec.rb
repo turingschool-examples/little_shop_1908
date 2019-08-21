b8b461d05a475399cade3274773c718b223fe3b1
Alec Wells <alec.wells47@gmail.com>
2019-08-21 12:55:18 -0600

    Add cart indicator pic

---
+++ spec/features/cart/indicator_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+describe "Cart Indicator" do
+  it 'I see a cart indicator in the navigation bar' do
+    visit '/'
+
+    within "#cart-indicator" do
+      expect(page).to  have_css("img[src*='https://cdn0.iconfinder.com/data/icons/shopping-cart-26/1000/Shopping-Basket-03-512.png']")
+    end
+  end
+end
