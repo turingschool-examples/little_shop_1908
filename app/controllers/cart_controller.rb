b019509f7688deca45adb5ae31247c10807d4cbe
Nancy Lee <srlee713@gmail.com>
2019-08-21 14:47:28 -0600

    Add show method in cart controller
    Co-authored-by: Josh Sherwood <49769068+joshsherwood1@users.noreply.github.com>

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -3,7 +3,15 @@ class CartController<ApplicationController
 
   def add_item
     item = Item.find(params[:item_id])
-    flash[:success] = "You now have added #{item.name} in your cart."
+    item_id_str = item.id.to_s
+    session[:cart] ||= Hash.new(0)
+    session[:cart][item_id_str] ||= 0
+    session[:cart][item_id_str] = session[:cart][item_id_str] + 1
+    quantity = session[:cart][item_id_str]
+    flash[:notice] = "You now have #{pluralize(quantity, "copy")} of #{item.name} in your cart."
     redirect_to "/items"
+  end
+
+  def show
   end
 end
