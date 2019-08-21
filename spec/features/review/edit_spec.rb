365a9929b87956989d4883d6664fb289ff51f30d
Leiya Kenney <leiya.kenney@gmail.com>
2019-08-21 14:23:03 -0600

    Updates test for editing review
    Co-authored-by: Kelly Sandeson kellysandeson@gmail.com

--- spec/features/review/edit_spec.rb
+++ spec/features/review/edit_spec.rb
@@ -22,26 +22,28 @@ RSpec.describe "Edit A Review" do
     it "When I visit an item's show page, I see a link to edit the review next to each review." do
       visit item_path(@chain)
 
-      click_link "Edit Review"
-      expect(current_path).to eq(edit_review_path(@chain))
+      within("#review-#{@review_1.id}") do
+        click_link "Edit Review"
+        expect(current_path).to eq(edit_review_path(@chain, @review_1))
+      end
     end
 
-    it "On this new page, I see a form that includes my review's title, numeric rating, and text and can update any of these fields and submit the form." do
-      visit new_review_path(@chain)
-      # save_and_open_page
-      expect(page).to have_content("Create A Review")
-      expect(page).to have_content("Headline")
-      expect(page).to have_content("Rating (1-5)")
-      expect(page).to have_content("Write Your Review")
-    end
-
-    it "When the form is submitted, I should return to that item's show page and I should see my updated review." do
-      visit new_review_path(@chain)
-      # save_and_open_page
-      expect(page).to have_content("Create A Review")
-      expect(page).to have_content("Headline")
-      expect(page).to have_content("Rating (1-5)")
-      expect(page).to have_content("Write Your Review")
-    end
+    # it "On this new page, I see a form that includes my review's title, numeric rating, and text and can update any of these fields and submit the form." do
+    #   visit edit_review_path(@chain)
+    #
+    #   expect(page).to have_content("Edit Your Review")
+    #   expect(page).to have_content("Headline")
+    #   expect(page).to have_content("Thoroughly Meh")
+    #   expect(page).to have_content("Rating (1-5)")
+    #   expect(page).to have_content("Rating: 2")
+    #   expect(page).to have_content("Write Your Review")
+    #   expect(page).to have_content("It seems like it's not the sturdiest chain. Will udpate after 6 months to see how it holds up.")
+    # end
+    #
+    # it "When the form is submitted, I should return to that item's show page and I should see my updated review." do
+    #   visit item_path(@chain)
+    #
+    #   expect(current_path).to eq(item_path(@chain))
+    # end
   end
 end
