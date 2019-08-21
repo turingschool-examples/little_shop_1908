22f2c977a9ab5a5f9abefd50cf4d28b735b5cf44
MackHalliday <mackenzie.halliday@gmail.com>
2019-08-21 13:07:51 -0600

    User Story 7 passing test

--- app/models/review.rb
+++ app/models/review.rb
@@ -8,4 +8,13 @@ class Review < ApplicationRecord
   def self.avg_rating
       average(:rating)
   end
+
+  def self.top_three_reviews
+
+    order(rating: :asc).last(3)
+  end
+
+  def self.bottom_three_reviews
+    order(rating: :asc).first(3)
+  end
 end
--- spec/features/reviews/index_spec.rb
+++ spec/features/reviews/index_spec.rb
@@ -58,21 +58,6 @@ RSpec.describe 'From Item Show Page', type: :feature do
       @review_10 = @tire.reviews.create(title: 'Review Title 10', content: "Content 10", rating: 5)
       @review_11 = @tire.reviews.create(title: 'Review Title 11', content: "Content 11", rating: 1)
       @review_12 = @tire.reviews.create(title: 'Review Title 12', content: "Content 12", rating: 1)
-
-      @dog_shop = Merchant.create(name: "Brian's Dog Shop", address: '125 Doggo St.', city: 'Denver', state: 'CO', zip: 80210)
-      @pull_toy = @dog_shop.items.create(name: "Pull Toy", description: "Great pull toy!", price: 10, image: "http://lovencaretoys.com/image/cache/dog/tug-toy-dog-pull-9010_2-800x800.jpg", inventory: 32)
-      @review_13 = @pull_toy.reviews.create(title: 'Review Title 13', content: "Content 13", rating: 5)
-      @review_14 = @pull_toy.reviews.create(title: 'Review Title 14', content: "Content 14", rating: 5)
-      @review_15 = @pull_toy.reviews.create(title: 'Review Title 15', content: "Content 15", rating: 4)
-      @review_16 = @pull_toy.reviews.create(title: 'Review Title 16', content: "Content 16", rating: 4)
-      @review_17 = @pull_toy.reviews.create(title: 'Review Title 17', content: "Content 17", rating: 3)
-      @review_18 = @pull_toy.reviews.create(title: 'Review Title 18', content: "Content 18", rating: 3)
-      @review_19 = @pull_toy.reviews.create(title: 'Review Title 19', content: "Content 19", rating: 2)
-      @review_20 = @pull_toy.reviews.create(title: 'Review Title 20', content: "Content 20", rating: 2)
-      @review_21 = @pull_toy.reviews.create(title: 'Review Title 21', content: "Content 21", rating: 1)
-      @review_22 = @pull_toy.reviews.create(title: 'Review Title 22', content: "Content 22", rating: 1)
-      @review_23 = @pull_toy.reviews.create(title: 'Review Title 23', content: "Content 23", rating: 5)
-      @review_24 = @pull_toy.reviews.create(title: 'Review Title 24', content: "Content 24", rating: 5)
     end
 
     it "show the top three reviews for this item" do
@@ -80,7 +65,7 @@ RSpec.describe 'From Item Show Page', type: :feature do
       visit "/items/#{@tire.id}"
 
       within '#reviews-stat-section' do
-    
+        #binding.pry
         expect(page).to have_content(@review_5.title)
         expect(page).to have_content(@review_10.title)
         expect(page).to have_content(@review_4.title)
@@ -95,6 +80,7 @@ RSpec.describe 'From Item Show Page', type: :feature do
       visit "/items/#{@tire.id}"
 
       within '#reviews-stat-section' do
+        #binding.pry
         expect(page).to have_content(@review_1.title)
         expect(page).to have_content(@review_6.title)
         expect(page).to have_content(@review_11.title)
