9b346d7055199bad8c49387c04b12fa5376563fe
Corina Allen <StarPerfect@gmail.com>
2019-08-20 18:22:35 -0600

    Complete User Story 8

--- spec/features/reviews/edit_spec.rb
+++ spec/features/reviews/edit_spec.rb
@@ -15,25 +15,25 @@ RSpec.describe 'Edit an Existing Review' do
   describe 'As a visitor' do
     describe 'When I see a review on an Item Show Page' do
       describe 'I can click on a link to Edit the review' do
-        it 'I then see a prepopulated form with the current review details to edit' do
-          before :each do
-            @bike_shop = Merchant.create(name: "Meg's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
-            @tire = @bike_shop.items.create(name: "Gatorskins", description: "They'll never pop!", price: 100, image: "https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588", inventory: 12)
-            @corina = @tire.reviews.create(title: 'Never Buy This Tire', content: "I bought two of these and they blew within a week of each other, a month after purchase", rating: 1)
-          end
+        before :each do
+          @bike_shop = Merchant.create(name: "Meg's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
+          @tire = @bike_shop.items.create(name: "Gatorskins", description: "They'll never pop!", price: 100, image: "https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588", inventory: 12)
+          @corina = @tire.reviews.create(title: 'Never Buy This Tire', content: "I bought two of these and they blew within a week of each other, a month after purchase", rating: 1)
 
           visit "/items/#{@tire.id}"
 
-          within
-          expect(page).to have_link("Edit Review")
+          within "#review-#{review.id}" do
+            expect(page).to have_link("Edit Review")
           end
 
-          click_on 'Edit Review'
+          it 'I then see a prepopulated form with the current review details to edit' do
+            click_on 'Edit Review'
 
-          expect(current_path).to eq("/reviews/#{@corina.id}/edit")
-          expect(find_field('Title').value).to eq "Never Buy This Tire"
-          expect(find_field('Review').value).to eq 'I bought two of these and they blew within a week of each other, a month after purchase'
-          expect(find_field('Rating').value).to eq 1
+            expect(current_path).to eq("/reviews/#{@corina.id}/edit")
+            expect(find_field('Title').value).to eq "Never Buy This Tire"
+            expect(find_field('Review').value).to eq 'I bought two of these and they blew within a week of each other, a month after purchase'
+            expect(find_field('Rating').value).to eq 1
+          end
         end
       end
     end
