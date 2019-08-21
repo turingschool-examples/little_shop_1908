d5e46fb7cd6821034238d00cdb0e6d9c25b83cb3
Leiya Kenney <leiya.kenney@gmail.com>
2019-08-21 14:31:03 -0600

    Writes test for prepopulated update review form
    Co-authored-by: Kelly Sandeson kellysandeson@gmail.com

--- spec/features/review/edit_spec.rb
+++ spec/features/review/edit_spec.rb
@@ -28,18 +28,16 @@ RSpec.describe "Edit A Review" do
       end
     end
 
-    # it "On this new page, I see a form that includes my review's title, numeric rating, and text and can update any of these fields and submit the form." do
-    #   visit edit_review_path(@chain)
-    #
-    #   expect(page).to have_content("Edit Your Review")
-    #   expect(page).to have_content("Headline")
-    #   expect(page).to have_content("Thoroughly Meh")
-    #   expect(page).to have_content("Rating (1-5)")
-    #   expect(page).to have_content("Rating: 2")
-    #   expect(page).to have_content("Write Your Review")
-    #   expect(page).to have_content("It seems like it's not the sturdiest chain. Will udpate after 6 months to see how it holds up.")
-    # end
-    #
+    it "On this new page, I see a form that includes my review's title, numeric rating, and text and can update any of these fields and submit the form." do
+      visit edit_review_path(@chain, @review_1)
+
+      expect(page).to have_content("Edit Your Review")
+      expect(page).to have_content("Headline")
+      expect(page).to have_content("Rating (1-5)")
+      expect(page).to have_content("Write Your Review")
+      expect(page).to have_button("Update Your Review")
+    end
+
     # it "When the form is submitted, I should return to that item's show page and I should see my updated review." do
     #   visit item_path(@chain)
     #
