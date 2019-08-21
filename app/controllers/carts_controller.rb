6f4bfb7e41f209230a64ad5ec9b1ac077adc9bb7
Evette Telyas <etelyas@gmail.com>
2019-08-21 00:07:19 -0600

    refactor carts_controller

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -8,9 +8,9 @@ class CartsController < ApplicationController
     if item.inventory > 0
       session[:cart][item_id_str] = session[:cart][item_id_str] + 1
       flash[:notice] = "1 #{item.name} has been added. You now have #{session[:cart][item_id_str]} #{item.name} in your cart."
+      item.buy
     else flash[:notice] = "There are not enough #{item.name} to add to yo cart, sry."
     end
-    item.buy
     redirect_to '/items'
   end
 
