48047c0645d0aca1eb20d824484f61fd31006b66
Nancy Lee <srlee713@gmail.com>
2019-08-21 14:46:52 -0600

    Add methods to Cart class
    Co-authored-by: Josh Sherwood <49769068+joshsherwood1@users.noreply.github.com>

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,2 +1,20 @@
 class Cart
+  attr_reader :contents
+
+  def initialize(contents)
+    if contents
+      @contents = contents
+    else
+      @contents = {}
+    end
+  end
+
+  def quantity_of(item_id)
+    @contents[item_id.to_s].to_i
+  end
+
+  def add_item(item_id)
+    @contents[item_id.to_s] = quantity_of(item_id) + 1
+  end
+
 end
