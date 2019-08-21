cf6f5877c4a5574fb4b0eb304dd4f7af502a520f
Leiya Kenney <leiya.kenney@gmail.com>
2019-08-21 14:31:03 -0600

    Updates Cart model
    Co-authored-by: Kelly Sandeson kellysandeson@gmail.com
    Co-Authored-By: Kelly Sandeson <ksandy95@users.noreply.github.com>

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,14 +1,25 @@
 class Cart
+  attr_accessor :contents
 
-  def initialize(contents)
-    if contents
-      @contents = contents
-    else
-      @contents = {}
-    end
+  def initialize(initial_contents)
+    # if contents
+    #   @contents = contents
+    # else
+    #   @contents = {}
+    # end
+    @contents = initial_contents || Hash.new(0)
   end
 
-  def items_count
-  end 
+  def add_item(item)
+    @contents[item.to_s] = quantity_of(item) + 1
+  end
+
+  def quantity_of(item)
+    @contents[item.to_s].to_i
+  end
+
+  def total_count
+    @contents.values.sum
+  end
 
 end
