2b12290b7cff95b3f92a0d3c1f38f1bcf6e161ff
Fenton Taylor <fentontaylor@gmail.com>
2019-08-21 13:49:38 -0600

    Add test for Add to Cart button, update routes, create cart controller
    Co-authored-by: Alec Wells <23268508+alect47@users.noreply.github.com>

---
+++ spec/features/cart/add_button_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+describe "User visits items show page and clicks Add to Cart" do
+  it 'Redirects them to items index with flash message about updated cart' do
+    dog_shop = Merchant.create(name: "Brian's Dog Shop", address: '125 Doggo St.', city: 'Denver', state: 'CO', zip: 80210)
+    pull_toy = dog_shop.items.create(name: "Pull Toy", description: "Great pull toy!", price: 10, image: "http://lovencaretoys.com/image/cache/dog/tug-toy-dog-pull-9010_2-800x800.jpg", inventory: 32)
+
+    visit "/items/#{pull_toy.id}"
+    expect(page).to have_link('Add to Cart')
+
+    click_link 'Add to Cart'
+
+    expect(current_path).to eq('/items')
+    expect(page).to have_content("#{pull_toy.name} added to cart.")
+  end
+
+  it 'Increments the cart count in the cart indicator' do
+
+  end
+end
+# As a visitor
+# When I visit an item's show page from the items index
+# I see a link or button to add this item to my cart
+# And I click this link or button
+# I am returned to the item index page
+# I see a flash message indicating the item has been added to my cart
+# The cart indicator in the navigation bar increments my cart count
