8d7b8392a5d135675b581aa7517e085e4e577d1e
Leiya Kenney <leiya.kenney@gmail.com>
2019-08-22 14:49:37 -0600

    Updates cart controller/model/view to include adding multiple items to cart
    Co-authored-by: Kelly Sandeson kellysandeson@gmail.com
    Co-Authored-By: Kelly Sandeson <ksandy95@users.noreply.github.com>

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -1,13 +1,12 @@
 class CartController < ApplicationController
 
-  def index
+  def show
+    @contents = cart.contents
   end
 
   def add_item
-    cart = Cart.new(session[:format])
     item = Item.find(params[:id])
     cart.add_item(item.id)
-    session[:cart] ||= Hash.new(0)
     session[:cart] = cart.contents
     flash[:success] = "#{item.name} has been added to your cart!"
     redirect_to items_path
--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,25 +1,28 @@
 class Cart
   attr_accessor :contents
 
-  def initialize(initial_contents)
-    # if contents
-    #   @contents = contents
-    # else
-    #   @contents = {}
-    # end
-    @contents = initial_contents || Hash.new(0)
+  def initialize(contents)
+    @contents = contents ||= Hash.new(0)
+    @contents.default = 0
   end
 
   def add_item(item)
-    @contents[item.to_s] = quantity_of(item) + 1
+    # binding.pry
+    @contents[item.to_s] +=  1
   end
 
   def quantity_of(item)
-    @contents[item.to_s].to_i
+    @contents[item.to_s]
   end
 
   def total_count
     @contents.values.sum
+  end
+
+  def items
+    @contents.keys.map do |item_id|
+      Item.find(item_id)
+    end
   end
 
 end
