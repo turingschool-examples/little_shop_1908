9101846cf47c3c0b23e8a5184e236a4e962ed783
Nancy Lee <srlee713@gmail.com>
2019-08-22 15:48:28 -0600

    Add decrease_quantity action in cart controller
    Co-authored-by: Josh Sherwood <49769068+joshsherwood1@users.noreply.github.com>

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -38,4 +38,16 @@ class CartController<ApplicationController
     end
     redirect_to "/cart"
   end
+
+  def decrease_quantity
+    item_id = Item.find(params[:item_id]).id
+    @cart = Cart.new(session[:cart])
+
+    if @cart.quantity_of(item_id) == 0
+      @cart.contents.delete(item_id.to_s)
+    else
+      @cart.contents[item_id.to_s] -= 1
+    end
+    redirect_to "/cart"
+  end
 end
