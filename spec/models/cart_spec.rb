78089b25178322358c8fd2871b79d7e97964f96e
Corina Allen <StarPerfect@gmail.com>
2019-08-22 18:33:34 -0600

    Cart Model

--- spec/models/cart_spec.rb
+++ spec/models/cart_spec.rb
@@ -1,3 +1,10 @@
+# As a visitor
+# When I visit an item's show page from the items index
+# I see a link or button to add this item to my cart
+# And I click this link or button
+# I am returned to the item index page
+# I see a flash message indicating the item has been added to my cart
+# The cart indicator in the navigation bar increments my cart count
 require 'rails_helper'
 
 RSpec.describe Cart do
