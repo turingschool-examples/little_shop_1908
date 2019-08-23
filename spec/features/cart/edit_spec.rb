be5fad741d1fe0672d1cb2270347fa2755bd2c2a
Leiya Kenney <leiya.kenney@gmail.com>
2019-08-22 18:54:23 -0600

    Revert "Updates cart tests to include removing singular items from cart"
    This reverts commit c25b74d969dd8e7cb095ff02b5d192b066c841cf.

--- spec/features/cart/edit_spec.rb
+++ spec/features/cart/edit_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe "Updating Cart" do
+RSpec.describe "Emptying Cart" do
   describe "should empty the cart" do
     before :each do
       @bike_shop = Merchant.create(name: "Brian's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
@@ -54,44 +54,6 @@ RSpec.describe "Updating Cart" do
       click_link("Empty Cart")
 
       expect(page).to have_content("Cart: 0")
-    end
-
-    it "When I have items in my cart and I visit my cart, next to each item in my cart, I see a button or link to remove that item from my cart" do
-      visit item_path(@chain.id)
-      click_on "Add To Cart"
-      visit item_path(@tire.id)
-      click_on "Add To Cart"
-
-      visit "/cart"
-
-      within "#item-#{@chain.id}" do
-        expect(page).to have_button("Remove From Cart")
-      end
-
-      within "#item-#{@tire.id}" do
-        expect(page).to have_button("Remove From Cart")
-      end
-    end
-
-    it "Clicking this button will remove the item but not other items" do
-      visit item_path(@chain.id)
-      click_on "Add To Cart"
-      visit item_path(@tire.id)
-      click_on "Add To Cart"
-
-      visit "/cart"
-
-      within "#item-#{@chain.id}" do
-        click_button("Remove From Cart")
-      end
-
-      expect(page).not_to have_content(@chain.name)
-
-      within "#item-#{@tire.id}" do
-        click_button("Remove From Cart")
-      end
-
-      expect(page).not_to have_content(@tire.name)
     end
   end
 end
