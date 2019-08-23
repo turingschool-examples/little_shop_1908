5d436ae08084c1db9c9bdef2775766430c64160e
Fenton Taylor <fentontaylor@gmail.com>
2019-08-22 20:22:49 -0600

    Remove user story from test file
    Co-authored-by: Alec Wells <23268508+alect47@users.noreply.github.com>

--- spec/features/orders/new_spec.rb
+++ spec/features/orders/new_spec.rb
@@ -136,18 +136,3 @@ describe 'User clicks link to Place Order' do
     end
   end
 end
-
-# As a visitor
-# When I fill out all information on the new order page
-# And click on 'Create Order'
-# An order is created and saved in the database
-# And I am redirected to that order's show page with the following information:
-# - My name and address (shipping information)
-# - Details of the order:
-# - the name of the item
-# - the merchant I'm buying this item from
-# - the price of the item
-# - my desired quantity of the item
-# - a subtotal (price multiplied by quantity)
-# - a grand total of what everything in my cart will cost
-# - the date when the order was created
