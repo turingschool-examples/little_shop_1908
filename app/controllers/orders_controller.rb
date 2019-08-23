bae9d519c7bcf36c21b7a1224d515dff5a0a42ed
Fenton Taylor <fentontaylor@gmail.com>
2019-08-22 19:28:16 -0600

    Update routes and orders_controller, create orders show page
    Co-authored-by: Alec Wells <23268508+alect47@users.noreply.github.com>

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -5,7 +5,18 @@ class OrdersController < ApplicationController
   end
 
   def create
+    order = Order.create!(order_params)
+    redirect_to "/orders/#{order.id}"
+  end
+
+  def show
     
+  end
+
+  private
+
+  def order_params
+    params.permit(:name, :address, :city, :state, :zip)
   end
 
 end
