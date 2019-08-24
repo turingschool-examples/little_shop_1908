af2170919483cc8cf9cd78a9e8af94ce2e3e22a6
Fenton Taylor <fentontaylor@gmail.com>
2019-08-24 12:06:36 -0600

    Add test for Merchant #has_orders?
    Co-authored-by: Alec Wells <23268508+alect47@users.noreply.github.com>

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,10 +1,13 @@
 class Merchant <ApplicationRecord
-  has_many :items
-  
+  has_many :items, dependent: :destroy
+
   validates_presence_of :name,
                         :address,
                         :city,
                         :state,
                         :zip
 
+  def has_orders?
+    
+  end
 end
