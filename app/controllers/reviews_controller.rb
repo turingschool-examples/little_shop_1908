3e88b099ed4f26dc709dff0fe44bf6cd0e8a3ab8
MackHalliday <mackenzie.halliday@gmail.com>
2019-08-24 14:55:36 -0600

    Removed dead code

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,8 +1,4 @@
 class ReviewsController < ApplicationController
-  def index
-    @item = Item.find(params[:item_id])
-    @reviews = @item.reviews
-  end
 
   def new
     @item = Item.find(params[:item_id])
