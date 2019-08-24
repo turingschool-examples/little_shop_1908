a733719be3bace380f3c0fb26101ac8a3ee3d65b
Kelly Sandeson <kellysandeson@gmail.com>
2019-08-24 13:30:17 -0600

    Create create method
    Co-authored-by: Leiya Kenney
    Co-Authored-By: Leiya Kenney <leiya.kenney@gmail.com>

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -5,7 +5,30 @@ class OrdersController < ApplicationController
   end
 
   def show
+    @order = Order.find(params[:id])
+  end
 
+  def create
+    new_order = Order.create(order_params)
+    cart.contents.each do |item_id, quantity|
+      item = Item.find(item_id)
+      name = item.name
+      merchant = item.merchant.name
+      price = item.price
+      subtotal = price * quantity
+      new_order.order_items.create(item_id: item_id, name: name, merchant: merchant, price: price, quantity: quantity, subtotal: subtotal)
+    end
+    if new_order.save
+      session[:cart] = nil
+      flash[:notice] = "Your order has been created!"
+      redirect_to "/orders/#{new_order.id}"
+    end
+  end
+
+  private
+
+  def order_params
+    params.permit(:name, :address, :city, :state, :zip)
   end
 
 end
