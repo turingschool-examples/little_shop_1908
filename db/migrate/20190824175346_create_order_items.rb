5585866af9680d4b3ea4219befb183f2e14338a5
Kelly Sandeson <kellysandeson@gmail.com>
2019-08-24 13:31:57 -0600

    Create order items table
    Co-authored-by: Leiya Kenney
    Co-Authored-By: Leiya Kenney <leiya.kenney@gmail.com>

---
+++ db/migrate/20190824175346_create_order_items.rb
@@ -0,0 +1,15 @@
+class CreateOrderItems < ActiveRecord::Migration[5.1]
+  def change
+    create_table :order_items do |t|
+      t.references :order, foreign_key: true
+      t.references :item, foreign_key: true
+      t.integer :quantity
+      t.float :price
+      t.string :name
+      t.string :merchant
+      t.float :subtotal
+
+      t.timestamps
+    end
+  end
+end
