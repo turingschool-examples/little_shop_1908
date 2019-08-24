1348169a4f9e1a6acea75f7e7615b3ebe4dbd1e1
Kelly Sandeson <kellysandeson@gmail.com>
2019-08-24 15:12:42 -0600

    Create sold items method
    Co-authored-by: Leiya Kenney
    Co-Authored-By: Leiya Kenney <leiya.kenney@gmail.com>

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,10 +1,15 @@
 class Merchant <ApplicationRecord
   has_many :items
-  
+
   validates_presence_of :name,
                         :address,
                         :city,
                         :state,
                         :zip
+
+  def sold_items?
+    merchants = OrderItem.all.pluck(:merchant_id)
+    merchants.include?(self.id)
+  end
 
 end
