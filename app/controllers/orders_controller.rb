745e1a34962bae87e15e11e9d21234a112504e25
Kelly Sandeson <kellysandeson@gmail.com>
2019-08-24 14:15:01 -0600

    Update create method for conditionals
    Co-authored-by: Leiya Kenney
    Co-Authored-By: Leiya Kenney <leiya.kenney@gmail.com>

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -9,19 +9,23 @@ class OrdersController < ApplicationController
   end
 
   def create
-    new_order = Order.create(order_params)
-    cart.contents.each do |item_id, quantity|
-      item = Item.find(item_id)
-      name = item.name
-      merchant = item.merchant.name
-      price = item.price
-      subtotal = price * quantity
-      new_order.order_items.create(item_id: item_id, name: name, merchant: merchant, price: price, quantity: quantity, subtotal: subtotal)
-    end
+    new_order = Order.new(order_params)
     if new_order.save
+      cart.contents.each do |item_id, quantity|
+        item = Item.find(item_id)
+        name = item.name
+        merchant = item.merchant.name
+        price = item.price
+        subtotal = price * quantity
+        new_order.order_items.create(item_id: item_id, name: name, merchant: merchant, price: price, quantity: quantity, subtotal: subtotal)
+      end
       session[:cart] = nil
       flash[:notice] = "Your order has been created!"
       redirect_to "/orders/#{new_order.id}"
+    else
+      flash[:notice] = "You must fill out all shipping information."
+      @cart = cart
+      render :new
     end
   end
 
