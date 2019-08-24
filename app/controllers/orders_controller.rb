daadf3d8f875923038378250e57467f530afb90f
Fenton Taylor <fentontaylor@gmail.com>
2019-08-24 10:25:20 -0600

    Add logic to orders_controller to create item_orders
    Co-authored-by: Alec Wells <23268508+alect47@users.noreply.github.com>

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -7,6 +7,14 @@ class OrdersController < ApplicationController
   def create
     order = Order.new(order_params)
     if order.save
+      cart = Cart.new(session[:cart])
+      total = cart.order_total
+      cart.contents.each do |item, qty|
+        ItemOrder.create(order_id: order.id,
+                        item_id: Item.find(item).id,
+                        quantity: qty,
+                        total_cost: total)
+      end
       redirect_to "/orders/#{order.id}"
     else
       flash[:error] = "Please fill in all the fields"
