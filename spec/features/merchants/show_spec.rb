5094fc70e721fef891eb02737907a7b87959c858
Leiya Kenney <leiya.kenney@gmail.com>
2019-08-26 12:04:19 -0600

    Makes test for more merchant statistics
    Co-authored-by: Kelly Sandeson kellysandeson@gmail.com
    Co-Authored-By: Kelly Sandeson <ksandy95@users.noreply.github.com>

--- spec/features/merchants/show_spec.rb
+++ spec/features/merchants/show_spec.rb
@@ -26,20 +26,24 @@ RSpec.describe 'merchant show page', type: :feature do
     it "When I visit a merchant's show page, I see statistics for that
     merchant, including: count of items for that merchant, average price of
     that merchant's items, and distinct cities where my items have been
-    ordered" do
+    ordered, and I see the top 3 highest rated items for that merchant (by average rating)" do
 
-      @chain = @bike_shop.items.create!(name: "Chain", description: "It'll never break!", price: 50, image: "https://www.rei.com/media/b61d1379-ec0e-4760-9247-57ef971af0ad?size=784x588", inventory: 5)
-      @tire = @bike_shop.items.create!(name: "Gatorskins", description: "They'll never pop!", price: 100, image: "https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588", inventory: 12)
-      @order_1 = Order.create!(name: "Leiya Kelly", address: '11 ILiveAtTuring Ave.', city: "Denver", state: "CO", zip: 80237)
-      @a = @order_1.order_items.create!(item_id: @chain.id, quantity: 1, price: @chain.price, name: @chain.name, merchant: @chain.merchant.name, subtotal: 50, merchant_id: @chain.merchant.id)
+      item_1 = @bike_shop.items.create!(name: "Chain", description: "It'll never break!", price: 50, image: "https://www.rei.com/media/b61d1379-ec0e-4760-9247-57ef971af0ad?size=784x588", inventory: 5)
+      item_2 = @bike_shop.items.create!(name: "Bike Lights", description: "So bright!", price: 25, image: "https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwiW-NjQv6HkAhUCt54KHYLkAJwQjRx6BAgBEAQ&url=https%3A%2F%2Fwww.amazon.com%2FCycle-Torch-Bolt-Combo-Rechargeable%2Fdp%2FB01N2HZV9U&psig=AOvVaw36ygs9M0LDALlZHfp40GLF&ust=1566941906310951", inventory: 7)
+      item_3 = @bike_shop.items.create!(name: "Gatorskins", description: "They'll never pop!", price: 100, image: "https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588", inventory: 12)
+
+      review_1 = item_1.reviews.create!(title: "Great", content: "I like this chain!", rating: 4)
+      review_2 = item_2.reviews.create!(title: "Win!", content: "These ARE lights!!", rating: 5)
+      review_3 = item_3.reviews.create!(title: "Yay", content: "I can ride my bike now!", rating: 5)
 
       visit "/merchants/#{@bike_shop.id}"
 
-      expect(page).to have_content(@bike_shop.count_of_items)
-      expect(page).to have_content("Items From This Merchant: 2")
-      expect(page).to have_content(@bike_shop.average_price)
-      expect(page).to have_content("Average Price: 75")
-      expect(page).to have_content("Cities: Denver")
+      expect(page).to have_content("#{@bike_shop.name}'s Top Three Items")
+      within ".top-reviews" do
+        expect(page).to have_content(item_1.name)
+        expect(page).to have_content(item_2.name)
+        expect(page).to have_content(item_3.name)
+      end
     end
   end
 end
