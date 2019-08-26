c4739dc940ac629485782a72ebd9a9faea06024c
Leiya Kenney <leiya.kenney@gmail.com>
2019-08-26 12:04:19 -0600

    Adds test for deleting item + reviews
    Co-authored-by: Kelly Sandeson kellysandeson@gmail.com
    Co-Authored-By: Kelly Sandeson <ksandy95@users.noreply.github.com>

--- spec/features/items/destroy_spec.rb
+++ spec/features/items/destroy_spec.rb
@@ -15,5 +15,21 @@ RSpec.describe 'item delete', type: :feature do
       expect(current_path).to eq("/items")
       expect(page).to_not have_css("#item-#{chain.id}")
     end
+
+    it "When I delete an item, all reviews associated with that item are also
+    deleted" do
+    bike_shop = Merchant.create(name: "Brian's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
+    chain = bike_shop.items.create(name: "Chain", description: "It'll never break!", price: 50, image: "https://www.rei.com/media/b61d1379-ec0e-4760-9247-57ef971af0ad?size=784x588", inventory: 5)
+    review_1 = Review.create(title: "Womp Womp", content: "I hate it", rating: 1, item_id: chain.id)
+
+    visit "/items/#{chain.id}"
+
+    expect(page).to have_content(review_1.content)
+
+    click_link "Delete Item"
+
+    review_1.destroyed?
+
+    end
   end
 end
