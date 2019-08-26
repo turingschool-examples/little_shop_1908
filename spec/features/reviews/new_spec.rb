66d27b79556f86a7293537a2e45850b040413733
Fenton Taylor <fentontaylor@gmail.com>
2019-08-26 11:54:06 -0600

    Add dependent destroy relationship for item reviews
    Co-authored-by: Alec Wells <23268508+alect47@users.noreply.github.com>

--- spec/features/reviews/new_spec.rb
+++ spec/features/reviews/new_spec.rb
@@ -5,7 +5,6 @@ describe 'User visits the item show page' do
     it 'Can create a new review' do
       bike_shop = Merchant.create(name: "Brian's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
       chain = bike_shop.items.create(name: "Chain", description: "It'll never break!", price: 50, image: "https://www.rei.com/media/b61d1379-ec0e-4760-9247-57ef971af0ad?size=784x588", inventory: 5)
-      # review = chain.reviews.create(title: "It's Great!", content: "Best chain ever!", rating: 3)
 
       visit "/items/#{chain.id}"
 
