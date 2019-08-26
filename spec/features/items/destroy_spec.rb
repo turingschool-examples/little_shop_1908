932cb425b1f16f79ea7aa19a0ad049ec530b55d2
Leiya Kenney <leiya.kenney@gmail.com>
2019-08-26 12:04:19 -0600

    Adds tests to not be able to delete an item with existing orders
    Co-authored-by: Kelly Sandeson kellysandeson@gmail.com
    Co-Authored-By: Kelly Sandeson <ksandy95@users.noreply.github.com>

--- spec/features/items/destroy_spec.rb
+++ spec/features/items/destroy_spec.rb
@@ -29,6 +29,18 @@ RSpec.describe 'item delete', type: :feature do
     click_link "Delete Item"
 
     review_1.destroyed?
+    end
+
+    it "If an item has been ordered, I can not delete that item, there is no link visible for me to delete the item" do
+      bike_shop = Merchant.create!(name: "Brian's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
+      chain = bike_shop.items.create!(name: "Chain", description: "It'll never break!", price: 50, image: "https://www.rei.com/media/b61d1379-ec0e-4760-9247-57ef971af0ad?size=784x588", inventory: 5)
+      order_1 = Order.create!(name: "Leiya Kelly", address: '11 ILiveAtTuring Ave.', city: "Denver", state: "CO", zip: 80237)
+      a = order_1.order_items.create!(item_id: chain.id, quantity: 1, price: chain.price, name: chain.name, merchant: chain.merchant.name, subtotal: 50, merchant_id: chain.merchant.id)
+
+      visit "/items/#{chain.id}"
+
+      expect(page).to_not have_link("Delete Item")
+
 
     end
   end
