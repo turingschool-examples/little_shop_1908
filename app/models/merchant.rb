55af243a2317b943416fe46326f44053a67ef2c9
Nancy Lee <srlee713@gmail.com>
2019-08-26 17:37:11 -0600

    Create total_items_count, average_price_items, and list_cities methods in merchant model
    Co-authored-by: Josh Sherwood <49769068+joshsherwood1@users.noreply.github.com>

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -8,4 +8,17 @@ class Merchant <ApplicationRecord
                         :state,
                         :zip
 
+  def total_items_count
+    items.count
+  end
+
+  def average_price_items
+    items.average(:price)
+  end
+
+  def list_cities
+    order_ids = items_orders.pluck(:order_id)
+    Order.where(id: order_ids).pluck("DISTINCT city")
+  end
+
 end
