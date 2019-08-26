3ee6bef473d44ddc9a1a7a9c17299dc3a5e87bc5
MackHalliday <mackenzie.halliday@gmail.com>
2019-08-26 13:59:55 -0600

    Add create method to order_controller

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,4 +2,17 @@ class OrdersController<ApplicationController
 
   def new
   end
+
+  def create
+    order = Order.create(order_params)
+    redirect_to "/orders/#{order.id}"
+  end
+
+  def show
+  end
+
+  private
+  def order_params
+    params.permit(:name,:address,:city,:state,:zip)
+  end
 end
