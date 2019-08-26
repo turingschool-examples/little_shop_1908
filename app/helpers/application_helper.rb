1b63532c1378c832323e6051a74a6d710096e3a3
Nancy Lee <srlee713@gmail.com>
2019-08-25 20:23:20 -0600

    Add bootstrap_class_for helper method to handle flash messages
    Co-authored-by: Josh Sherwood <49769068+joshsherwood1@users.noreply.github.com>

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,22 @@
 module ApplicationHelper
+  def bootstrap_class_for flash_type
+    case flash_type
+      when "success"
+        "alert-success"
+      when "error"
+        "alert-danger"
+      when "limit"
+        "alert-danger"
+      when "incomplete_order"
+        "alert-danger"
+      when "incomplete_merchant"
+        "alert-danger"
+      when "alert"
+        "alert-warning"
+      when "notice"
+        "alert-info"
+      else
+        flash_type.to_s
+    end
+  end
 end
