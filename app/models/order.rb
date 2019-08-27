35825634fb3a98d47ed817d0fc80477b0d5d04ce
MackHalliday <mackenzie.halliday@gmail.com>
2019-08-26 23:16:38 -0600

    Method to return item object with quantity for order

--- app/models/order.rb
+++ app/models/order.rb
@@ -9,7 +9,12 @@ class Order < ApplicationRecord
                         :state,
                         :zip
 
-  def self.order_details(order_id)
-    select(where(id: [order_id]))
+  def order_details
+
+    item_orders = ItemOrder.where('order_id' => self.id)
+
+    order_details = item_orders.each_with_object({}) do |item, hash|
+                      hash[Item.find(item.item_id)] = item.quantity
+                    end
   end
 end
