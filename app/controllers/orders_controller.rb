aca4eac9e5b7f19a3b55b105875a45d4b3cd6f32
Corina Allen <StarPerfect@gmail.com>
2019-08-27 15:39:42 -0600

    Add flash message to user when incomplete shipping information

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,15 +4,18 @@ class OrdersController<ApplicationController
   end
 
   def create
-    order = Order.create(order_params)
-
-    #MOVE INTO ITEM_ORDER MODEL
-    cart.item_quantity.each do |item, quantity|
-      ItemOrder.create(:order_id => order.id, :item_id => item.id, :quantity => quantity, :subtotal => item.item_subtotal(quantity))
+    order = Order.new(order_params)
+    if order.save
+      #MOVE INTO ITEM_ORDER MODEL
+      cart.item_quantity.each do |item, quantity|
+        ItemOrder.create(:order_id => order.id, :item_id => item.id, :quantity => quantity, :subtotal => item.item_subtotal(quantity))
+      end
+      reset_session
+      redirect_to "/orders/#{order.id}"
+    else
+      flash[:incomplete] = 'All fields must be completed to create a new order. Please try again.'
+      redirect_to '/orders/new'
     end
-
-    reset_session
-    redirect_to "/orders/#{order.id}"
   end
 
   def show
@@ -20,6 +23,7 @@ class OrdersController<ApplicationController
   end
 
   private
+
   def order_params
     params.permit(:name,:address,:city,:state,:zip)
   end
