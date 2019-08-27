81aa2e272911dfe4b493f5bf9641300985ef315f
Fenton Taylor <fentontaylor@gmail.com>
2019-08-27 14:26:31 -0600

    Add alternate render for item show with no reviews
    Co-authored-by: Alec Wells <23268508+alect47@users.noreply.github.com>

--- spec/features/items/show_spec.rb
+++ spec/features/items/show_spec.rb
@@ -29,7 +29,7 @@ RSpec.describe 'item show page', type: :feature do
           expect(page).to have_content("Rating: #{review.rating}")
         end
       end
-      
+
       within '.reviews-stats' do
         within '#top-three' do
           expect(page).to have_content(review_1.title)
@@ -56,6 +56,18 @@ RSpec.describe 'item show page', type: :feature do
           expect(page).to have_content("Average Rating: 2.75")
         end
       end
+    end
+  end
+
+  it "shows alternate reviews header if no reviews" do
+    bike_shop = Merchant.create(name: "Brian's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
+    chain = bike_shop.items.create(name: "Chain", description: "It'll never break!", price: 50, image: "https://www.rei.com/media/b61d1379-ec0e-4760-9247-57ef971af0ad?size=784x588", inventory: 5)
+
+    visit "/items/#{chain.id}"
+
+    within '.reviews-section-header' do
+      expect(page).to have_content('Customer Reviews: No reviews yet')
+      expect(page).to_not have_css(".dropbtn")
     end
   end
 end
