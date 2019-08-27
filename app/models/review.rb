14c351d91826445773effc367abd8ec385b8f7ab
Fenton Taylor <fentontaylor@gmail.com>
2019-08-27 15:54:16 -0600

    Add sort function for reviews
    Co-authored-by: Alec Wells <23268508+alect47@users.noreply.github.com>

--- app/models/review.rb
+++ app/models/review.rb
@@ -6,7 +6,7 @@ class Review < ApplicationRecord
   validates :rating, numericality: {only_integer: true,
                                     greater_than_or_equal_to: 1,
                                     less_than_or_equal_to: 5}
-                                    
+
   def self.top_or_bottom_three(item_id, order = :desc)
     where(item_id: item_id)
       .order(rating: order)
@@ -16,6 +16,19 @@ class Review < ApplicationRecord
   def self.average_rating(item_id)
     where(item_id: item_id)
       .average(:rating)
+  end
+
+  def self.sort_reviews(method, item_id)
+    reviews = Review.where(item_id: item_id)
+    if method == 'max-rating'
+      reviews.order(rating: :desc, created_at: :desc)
+    elsif method == 'min-rating'
+      reviews.order(rating: :asc, created_at: :desc)
+    elsif method == 'date-asc'
+      reviews.order(created_at: :asc)
+    else
+      reviews.order(created_at: :desc)
+    end
   end
 
 end
