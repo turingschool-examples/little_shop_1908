63898eca917151d98fc582cd7b6326fb82e51f1b
MackHalliday <mackenzie.halliday@gmail.com>
2019-08-27 21:44:00 -0600

    Add methods to find avg_price and num_products

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -9,4 +9,19 @@ class Merchant < ApplicationRecord
                         :state,
                         :zip
 
+  def num_products
+    self.items.count
+  end
+
+  def avg_price
+    if num_products > 0
+      price_sum = self.items.sum do |item|
+        item.price
+      end/num_products
+    end
+  end
+
+  def shipped_to_cities
+    order(rating: :asc).limit(3)
+  end
 end
