659c1a08ef833d9609dbfd02fd78e61a276dc992
Fenton Taylor <fentontaylor@gmail.com>
2019-08-28 11:41:56 -0600

    Add before each to cart test
    Co-authored-by: Alec Wells <23268508+alect47@users.noreply.github.com>

--- spec/models/cart_spec.rb
+++ spec/models/cart_spec.rb
@@ -46,20 +46,19 @@ describe Cart do
     expect(cart.contents).to eq({"7"=>1})
   end
 
+  before :each do
+    @dog_shop = Merchant.create(name: "Brian's Dog Shop", address: '125 Doggo St.', city: 'Denver', state: 'CO', zip: 80210)
+    @pull_toy = @dog_shop.items.create(name: "Pull Toy", description: "Great pull toy!", price: 10, image: "http://lovencaretoys.com/image/cache/dog/tug-toy-dog-pull-9010_2-800x800.jpg", inventory: 32)
+    @dog_bone = @dog_shop.items.create(name: "Dog Bone", description: "They'll love it!", price: 21, image: "https://img.chewy.com/is/image/catalog/54226_MAIN._AC_SL1500_V1534449573_.jpg", active?:false, inventory: 21)
+    @cart = Cart.new({@pull_toy.id.to_s=>1, @dog_bone.id.to_s=>2})
+  end
+
   it "can calculate subtotals" do
-    dog_shop = Merchant.create(name: "Brian's Dog Shop", address: '125 Doggo St.', city: 'Denver', state: 'CO', zip: 80210)
-    pull_toy = dog_shop.items.create(name: "Pull Toy", description: "Great pull toy!", price: 10, image: "http://lovencaretoys.com/image/cache/dog/tug-toy-dog-pull-9010_2-800x800.jpg", inventory: 32)
-    dog_bone = dog_shop.items.create(name: "Dog Bone", description: "They'll love it!", price: 21, image: "https://img.chewy.com/is/image/catalog/54226_MAIN._AC_SL1500_V1534449573_.jpg", active?:false, inventory: 21)
-    cart = Cart.new({pull_toy.id.to_s=>1, dog_bone.id.to_s=>2})
-    expect(cart.subtotal(pull_toy.id)).to eq(10)
-    expect(cart.subtotal(dog_bone.id)).to eq(42)
+    expect(@cart.subtotal(@pull_toy.id)).to eq(10)
+    expect(@cart.subtotal(@dog_bone.id)).to eq(42)
   end
 
   it "can calculate order total" do
-    dog_shop = Merchant.create(name: "Brian's Dog Shop", address: '125 Doggo St.', city: 'Denver', state: 'CO', zip: 80210)
-    pull_toy = dog_shop.items.create(name: "Pull Toy", description: "Great pull toy!", price: 10, image: "http://lovencaretoys.com/image/cache/dog/tug-toy-dog-pull-9010_2-800x800.jpg", inventory: 32)
-    dog_bone = dog_shop.items.create(name: "Dog Bone", description: "They'll love it!", price: 21, image: "https://img.chewy.com/is/image/catalog/54226_MAIN._AC_SL1500_V1534449573_.jpg", active?:false, inventory: 21)
-    cart = Cart.new({pull_toy.id.to_s=>1, dog_bone.id.to_s=>2})
-    expect(cart.order_total).to eq(52)
+    expect(@cart.order_total).to eq(52)
   end
 end
