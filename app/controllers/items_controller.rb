63c7e88c70e908626c15d8c2c94493edeff6e099
Alec Wells <alec.wells47@gmail.com>
2019-08-28 12:02:51 -0600

    Refactor items controller with before action

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,6 @@
 class ItemsController < ApplicationController
+  before_action :set_item, only: [:edit, :update, :destroy]
+  before_action :set_merchant, only: [:new, :create]
 
   def index
     if params[:merchant_id]
@@ -24,37 +26,32 @@ class ItemsController < ApplicationController
   end
 
   def new
-    @merchant = Merchant.find(params[:merchant_id])
   end
 
   def create
-    merchant = Merchant.find(params[:merchant_id])
-    item = merchant.items.new(item_params)
+    item = @merchant.items.new(item_params)
     if item.save
-      redirect_to "/merchants/#{merchant.id}/items"
+      redirect_to "/merchants/#{@merchant.id}/items"
     else
       flash[:error] = item.errors.full_messages
-      redirect_to "/merchants/#{merchant.id}/items/new"
+      redirect_to "/merchants/#{@merchant.id}/items/new"
     end
   end
 
   def edit
-    @item = Item.find(params[:id])
   end
 
   def update
-    item = Item.find(params[:id])
-    if item.update(item_params)
-      redirect_to "/items/#{item.id}"
+    if @item.update(item_params)
+      redirect_to "/items/#{@item.id}"
     else
-      flash[:error] = item.errors.full_messages
-      redirect_to "/items/#{item.id}/edit"
+      flash[:error] = @item.errors.full_messages
+      redirect_to "/items/#{@item.id}/edit"
     end
   end
 
   def destroy
-    item = Item.find(params[:id])
-    item.destroy
+    @item.destroy
     redirect_to "/items"
   end
 
@@ -62,6 +59,14 @@ class ItemsController < ApplicationController
 
   def item_params
     params.permit(:name, :description, :price, :inventory, :image)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
+  def set_merchant
+    @merchant = Merchant.find(params[:merchant_id])
   end
 
 end
