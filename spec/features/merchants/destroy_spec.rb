ad3003c2c495377980d05237e9ba37af0d1b4a89
Fenton Taylor <fentontaylor@gmail.com>
2019-08-28 07:07:33 -0600

    More test refactoring, additional sad path test for new merchant
    Co-authored-by: Alec Wells <23268508+alect47@users.noreply.github.com>

--- spec/features/merchants/destroy_spec.rb
+++ spec/features/merchants/destroy_spec.rb
@@ -3,10 +3,14 @@ require 'rails_helper'
 RSpec.describe "As a visitor" do
   describe "When a merchant has no orders" do
     describe "When I visit a merchant show page" do
-      it "I can delete a merchant" do
-        bike_shop = Merchant.create(name: "Brian's Bike Shop", address: '123 Bike Rd.', city: 'Richmond', state: 'VA', zip: 80203)
+      before :each do
+        @bike_shop = Merchant.create(name: "Brian's Bike Shop", address: '123 Bike Rd.', city: 'Richmond', state: 'VA', zip: 80203)
+        @chain = @bike_shop.items.create(name: "Chain", description: "It'll never break!", price: 50, image: "https://www.rei.com/media/b61d1379-ec0e-4760-9247-57ef971af0ad?size=784x588", inventory: 5)
+      end
 
-        visit "merchants/#{bike_shop.id}"
+      it "I can delete a merchant" do
+
+        visit "merchants/#{@bike_shop.id}"
 
         click_on "Delete Merchant"
 
@@ -15,22 +19,19 @@ RSpec.describe "As a visitor" do
       end
 
       it "When I delete a merchant, it deletes its items" do
-        bike_shop = Merchant.create(name: "Brian's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
-        chain = bike_shop.items.create(name: "Chain", description: "It'll never break!", price: 50, image: "https://www.rei.com/media/b61d1379-ec0e-4760-9247-57ef971af0ad?size=784x588", inventory: 5)
-
         visit '/merchants'
-        expect(page).to have_css("#merchant-#{bike_shop.id}")
+        expect(page).to have_css("#merchant-#{@bike_shop.id}")
 
-        visit "merchants/#{bike_shop.id}"
+        visit "merchants/#{@bike_shop.id}"
 
         click_on "Delete Merchant"
 
         expect(current_path).to eq('/merchants')
         expect(page).to_not have_content("Brian's Bike Shop")
-        expect(page).to_not have_css("#merchant-#{bike_shop.id}")
+        expect(page).to_not have_css("#merchant-#{@bike_shop.id}")
 
         visit "/items"
-        expect(page).to_not have_css("#item-#{chain.id}")
+        expect(page).to_not have_css("#item-#{@chain.id}")
       end
     end
   end
