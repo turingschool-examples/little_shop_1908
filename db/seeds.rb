1a041870fae63d67ef272bfd2bb559155a4f951b
MackHalliday <mackenzie.halliday@gmail.com>
2019-08-28 12:52:21 -0600

    Update seed file with orders and additional reviews

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,41 +1,75 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
 
 Review.destroy_all
 Item.destroy_all
 Merchant.destroy_all
 
 #merchants
-bike_shop = Merchant.create(name: "Meg's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
-dog_shop = Merchant.create(name: "Brian's Dog Shop", address: '125 Doggo St.', city: 'Denver', state: 'CO', zip: 80210)
+#shops
+@bike_shop = Merchant.create(name: "Meg's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
+@dog_shop = Merchant.create(name: "Brian's Dog Shop", address: '125 Doggo St.', city: 'Denver', state: 'CO', zip: 80210)
 
 #bike_shop items
-tire = bike_shop.items.create(name: "Gatorskins", description: "They'll never pop!", price: 100, image: "https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588", inventory: 12)
-tire_review = tire.reviews.create(title: 'Never Buy This Tire', content: "I bought two of these and they blew within a week of each other, a month after purchase", rating: 1)
+@tire = @bike_shop.items.create(name: "Gatorskins", description: "They'll never pop!", price: 100, image: "https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588", inventory: 12)
+@bike = @bike_shop.items.create(name: "Red Bike", description: "Oldie, but goodie", price: 200, image: "https://i.pinimg.com/originals/9d/5f/29/9d5f29749894957753a9edd9e2358d8b.png", inventory: 10)
 
 #dog_shop items
-pull_toy = dog_shop.items.create(name: "Pull Toy", description: "Great pull toy!", price: 10, image: "http://lovencaretoys.com/image/cache/dog/tug-toy-dog-pull-9010_2-800x800.jpg", inventory: 32)
-pull_toy_review = pull_toy.reviews.create(title: "Sparky love this!", content: "Sparky took it right off the shelf and began playing with it immediately so I bought him 2!", rating: 5)
+@pull_toy = @dog_shop.items.create(name: "Pull Toy", description: "Great pull toy!", price: 10, image: "http://lovencaretoys.com/image/cache/dog/tug-toy-dog-pull-9010_2-800x800.jpg", inventory: 32)
+@brush = @dog_shop.items.create(name: "Brush", description: "Great for long haired pets", price: 15, image: "https://images-na.ssl-images-amazon.com/images/I/71V8HaHa02L._SL1200_.jpg", inventory: 15)
 
-dog_bone = dog_shop.items.create(name: "Dog Bone", description: "They'll love it!", price: 21, image: "https://img.chewy.com/is/image/catalog/54226_MAIN._AC_SL1500_V1534449573_.jpg", active?:false, inventory: 21)
-dog_bone_review = dog_bone.reviews.create(title: 'What a dog bone!', content: "Taste great!", rating: 5)
-dog_bone_review_2 = dog_bone.reviews.create(title: 'Title Example', content: "Taste great!", rating: 5)
-# puts dog_bone_review_2.id
+#tire_reviews
+@review_1 = @tire.reviews.create(title: 'Review Title 1', content: "Content 1", rating: 1)
+@review_2 = @tire.reviews.create(title: 'Review Title 2', content: "Content 2", rating: 2)
+@review_3 = @tire.reviews.create(title: 'Review Title 3', content: "Content 3", rating: 3)
+@review_4 = @tire.reviews.create(title: 'Review Title 1', content: "Content 1", rating: 1)
+@review_5 = @tire.reviews.create(title: 'Review Title 2', content: "Content 2", rating: 2)
+@review_6 = @tire.reviews.create(title: 'Review Title 3', content: "Content 3", rating: 3)
 
-review_1 = tire.reviews.create(title: 'Review Title 1', content: "Content 1", rating: 1)
-review_2 = tire.reviews.create(title: 'Review Title 2', content: "Content 2", rating: 2)
-review_3 = tire.reviews.create(title: 'Review Title 3', content: "Content 3", rating: 3)
-review_4 = tire.reviews.create(title: 'Review Title 4', content: "Content 4", rating: 4)
-review_5 = tire.reviews.create(title: 'Review Title 5', content: "Content 5", rating: 5)
-review_6 = tire.reviews.create(title: 'Review Title 6', content: "Content 6", rating: 1)
-review_7 = tire.reviews.create(title: 'Review Title 7', content: "Content 7", rating: 2)
-review_8 = tire.reviews.create(title: 'Review Title 8', content: "Content 8", rating: 3)
-review_9 = tire.reviews.create(title: 'Review Title 9', content: "Content 9", rating: 4)
-review_10 = tire.reviews.create(title: 'Review Title 10', content: "Content 10", rating: 5)
-review_11 = tire.reviews.create(title: 'Review Title 11', content: "Content 11", rating: 1)
-review_12 = tire.reviews.create(title: 'Review Title 12', content: "Content 12", rating: 1)
+#bike_reviews
+@review_7 = @bike.reviews.create(title: 'Review Title 1', content: "Content 1", rating: 1)
+@review_8 = @bike.reviews.create(title: 'Review Title 2', content: "Content 2", rating: 4)
+@review_9 = @bike.reviews.create(title: 'Review Title 3', content: "Content 3", rating: 5)
+@review_10 = @bike.reviews.create(title: 'Review Title 1', content: "Content 1", rating: 1)
+@review_11 = @bike.reviews.create(title: 'Review Title 2', content: "Content 2", rating: 4)
+@review_12 = @bike.reviews.create(title: 'Review Title 3', content: "Content 3", rating: 5)
+
+#pull_toy_reviews
+@review_13 = @pull_toy.reviews.create(title: 'Review Title 1', content: "Content 1", rating: 1)
+@review_14 = @pull_toy.reviews.create(title: 'Review Title 2', content: "Content 2", rating: 2)
+@review_15 = @pull_toy.reviews.create(title: 'Review Title 3', content: "Content 3", rating: 5)
+@review_16 = @pull_toy.reviews.create(title: 'Review Title 1', content: "Content 1", rating: 4)
+@review_17 = @pull_toy.reviews.create(title: 'Review Title 2', content: "Content 2", rating: 5)
+@review_18 = @pull_toy.reviews.create(title: 'Review Title 3', content: "Content 3", rating: 5)
+
+#brush_reviews
+@review_19 = @brush.reviews.create(title: 'Review Title 1', content: "Content 1", rating: 5)
+@review_20 = @brush.reviews.create(title: 'Review Title 2', content: "Content 2", rating: 4)
+@review_21 = @brush.reviews.create(title: 'Review Title 3', content: "Content 3", rating: 1)
+@review_22 = @brush.reviews.create(title: 'Review Title 1', content: "Content 1", rating: 3)
+@review_23 = @brush.reviews.create(title: 'Review Title 2', content: "Content 2", rating: 2)
+@review_24 = @brush.reviews.create(title: 'Review Title 3', content: "Content 3", rating: 1)
+
+#user orders
+@order_1 = Order.create!(name: "Mack", address: "123 Happy St", city: "Denver", state: "CO", zip: "80205")
+  @item_order_1 = @order_1.item_orders.create!(order: @order_1, item: @bike, quantity: 1, subtotal: @bike.item_subtotal(1))
+
+@order_2 = Order.create(name: "John", address: "123 West St", city: "Golden", state: "CO", zip: "56600")
+  @item_order_2 = ItemOrder.create(order: @order_2, item: @tire, quantity: 12, subtotal: @tire.item_subtotal(12))
+  @item_order_3 = ItemOrder.create(order: @order_2, item: @brush, quantity: 3, subtotal: @brush.item_subtotal(3))
+
+@order_3 = Order.create(name: "Amber", address: "123 East St", city: "Denver", state: "CO", zip: "80205")
+  @item_order_4 = ItemOrder.create(order: @order_3, item: @pull_toy, quantity: 4, subtotal: @pull_toy.item_subtotal(4))
+  @item_order_5 = ItemOrder.create(order: @order_3, item: @brush, quantity: 3, subtotal: @brush.item_subtotal(3))
+  @item_order_6 = ItemOrder.create(order: @order_3, item: @tire, quantity: 1, subtotal: @tire.item_subtotal(1))
+
+@order_4 = Order.create(name: "Emily", address: "123 North St", city: "Golden", state: "CO", zip: "56601")
+  @item_order_7 = ItemOrder.create(order: @order_4, item: @tire, quantity: 1, subtotal: @tire.item_subtotal(1))
+  @item_order_8 = ItemOrder.create(order: @order_4, item: @bike, quantity: 1, subtotal: @bike.item_subtotal(1))
+
+@order_5 = Order.create(name: "Adam", address: "123 South St", city: "Chicago", state: "IL", zip: "61704")
+  @item_order_9 = ItemOrder.create(order: @order_5, item: @pull_toy, quantity: 5, subtotal: @pull_toy.item_subtotal(5))
+  @item_order_10 = ItemOrder.create(order: @order_5, item: @tire, quantity: 2, subtotal: @tire.item_subtotal(2))
+
+@order_6 = Order.create(name: "Matt", address: "123 Road St", city: "Chicago", state: "IL", zip: "61704")
+  @item_order_11 = ItemOrder.create(order: @order_6, item: @bike, quantity: 2, subtotal: @bike.item_subtotal(2))
+  @item_order_12 = ItemOrder.create(order: @order_6, item: @brush, quantity: 1, subtotal: @brush.item_subtotal(1))
