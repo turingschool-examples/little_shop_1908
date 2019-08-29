a788b0d2016011e928df805130b820804b2a9717
Leiya Kenney <leiya.kenney@gmail.com>
2019-08-29 08:17:42 -0600

    Works on getting Heroku working with dependencies

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,6 @@
 class Item <ApplicationRecord
   belongs_to :merchant
-  has_many :reviews, :dependent => :destroy
+  has_many :reviews, dependent: :destroy
   has_many :order_items
   has_many :orders, through: :order_items
 
