a25714577482ee03f6d89b03f0eee6cbbbcd4e26
Fenton Taylor <fentontaylor@gmail.com>
2019-08-28 19:38:38 -0600

    Add test for alternative text on merchant stats
    Co-authored-by: Alec Wells <23268508+alect47@users.noreply.github.com>

---
+++ spec/features/merchants/show_alternative_stats_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+describe 'User visits merchant page with no items or orders' do
+  it "Shows alternative text instead of blanks" do
+    bob = Merchant.create(name: 'Bob Ross Supplies', address: '123 Happy Tree St.', city: 'Lalaland', state: 'CA', zip: 90827)
+
+    visit "/merchants/#{bob.id}"
+
+    within '.merchant-stats' do
+      expect(page).to have_content("Count of items: 0")
+      expect(page).to have_content("Average price: No items for #{bob.name} yet")
+      expect(page).to have_content("Customer locations: #{bob.name} has not shipped any orders yet")
+      within '.top-reviewed-items' do
+        expect(page).to have_content("No items for #{bob.name} yet")
+      end
+    end
+  end
+end
