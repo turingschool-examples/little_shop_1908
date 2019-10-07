3c1dcae3160f7f8b9887ef1410256916d627678e
Michael Cooper <macoop2363@gmail.com>
2019-10-07 16:46:54 -0600

    Add reviews table
    Co-authored-by: John Travers <46035439+johnktravers@users.noreply.github.com>

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190817190207) do
+ActiveRecord::Schema.define(version: 20191007215131) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,5 +38,16 @@ ActiveRecord::Schema.define(version: 20190817190207) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "reviews", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.integer "rating"
+    t.bigint "item_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["item_id"], name: "index_reviews_on_item_id"
+  end
+
   add_foreign_key "items", "merchants"
+  add_foreign_key "reviews", "items"
 end
