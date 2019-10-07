d551362d210fb1eac680da0fcb0495e8287e0b40
Laura Schulz <lrs8810@gmail.com>
2019-10-07 16:56:33 -0600

    Add review item show spec
    Co-Authored-By: Mary Lang <mcat56@users.noreply.github.com>

--- spec/features/items/show_spec.rb
+++ spec/features/items/show_spec.rb
@@ -4,8 +4,8 @@ RSpec.describe 'item show page', type: :feature do
   before(:each) do
     @bike_shop = Merchant.create(name: "Brian's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
     @chain = @bike_shop.items.create(name: "Chain", description: "It'll never break!", price: 50, image: "https://www.rei.com/media/b61d1379-ec0e-4760-9247-57ef971af0ad?size=784x588", inventory: 5)
-    @good_review = @chain.reviews.create(title: "Awesome chain!", content: "This was a great chain! Would buy again.", rating: 5)
     @bad_review = @chain.reviews.create(title: "Worst chain!", content: "NEVER buy this chain.", rating: 1)
+    @good_review = @chain.reviews.create(title: "Awesome chain!", content: "This was a great chain! Would buy again.", rating: 5)
 
     visit "/items/#{@chain.id}"
   end
@@ -23,15 +23,15 @@ RSpec.describe 'item show page', type: :feature do
 
   it 'shows list of review for that item' do
     within "#review-#{@good_review.id}" do
-      expect(page).to have_content(@good_review.title)
-      expect(page).to have_content(@good_review.content)
-      expect(page).to have_content(@good_review.rating)
+      expect(page).to have_content("Awesome chain!")
+      expect(page).to have_content("This was a great chain! Would buy again.")
+      expect(page).to have_content("Rating: 5")
     end
 
     within  "#review-#{@bad_review.id}" do
-      expect(page).to have_content(@bad_review.title)
-      expect(page).to have_content(@bad_review.content)
-      expect(page).to have_content(@bad_review.rating)
+      expect(page).to have_content("Worst chain!")
+      expect(page).to have_content("NEVER buy this chain.")
+      expect(page).to have_content("Rating: 1")
     end
   end
 end
