2867ccac584c557189f78b15a7450d98b3e1effd
Laura Schulz <lrs8810@gmail.com>
2019-10-07 16:05:51 -0600

    Add reviews to item show spec and refactor before each
    Co-Authored-By: Mary Lang <mcat56@users.noreply.github.com>

--- spec/features/items/show_spec.rb
+++ spec/features/items/show_spec.rb
@@ -1,19 +1,37 @@
 require 'rails_helper'
 
 RSpec.describe 'item show page', type: :feature do
+  before(:each) do
+    @bike_shop = Merchant.create(name: "Brian's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
+    @chain = @bike_shop.items.create(name: "Chain", description: "It'll never break!", price: 50, image: "https://www.rei.com/media/b61d1379-ec0e-4760-9247-57ef971af0ad?size=784x588", inventory: 5)
+    @good_review = @chain.reviews.create(title: "Awesome chain!", content: "This was a great chain! Would buy again.", rating: 5)
+    @bad_review = @chain.reviews.create(title: "Worst chain!", content: "NEVER buy this chain.", rating: 1)
+
+    visit "/items/#{@chain.id}"
+  end
+
   it 'shows item info' do
-    bike_shop = Merchant.create(name: "Brian's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
-    chain = bike_shop.items.create(name: "Chain", description: "It'll never break!", price: 50, image: "https://www.rei.com/media/b61d1379-ec0e-4760-9247-57ef971af0ad?size=784x588", inventory: 5)
-
-    visit "items/#{chain.id}"
-
-    expect(page).to have_link(chain.merchant.name)
-    expect(page).to have_content(chain.name)
-    expect(page).to have_content(chain.description)
-    expect(page).to have_content("Price: $#{chain.price}")
+    expect(page).to have_link(@chain.merchant.name)
+    expect(page).to have_content(@chain.name)
+    expect(page).to have_content(@chain.description)
+    expect(page).to have_content("Price: $#{@chain.price}")
     expect(page).to have_content("Active")
-    expect(page).to have_content("Inventory: #{chain.inventory}")
-    expect(page).to have_content("Sold by: #{bike_shop.name}")
-    expect(page).to have_css("img[src*='#{chain.image}']")
+    expect(page).to have_content("Inventory: #{@chain.inventory}")
+    expect(page).to have_content("Sold by: #{@bike_shop.name}")
+    expect(page).to have_css("img[src*='#{@chain.image}']")
+  end
+
+  it 'shows list of review for that item' do
+    within "#review-#{@good_review.id}" do
+      expect(page).to have_content(@good_review.title)
+      expect(page).to have_content(@good_review.content)
+      expect(page).to have_content(@good_review.rating)
+    end
+
+    within  "#review-#{@bad_review.id}" do
+      expect(page).to have_content(@bad_review.title)
+      expect(page).to have_content(@bad_review.content)
+      expect(page).to have_content(@bad_review.rating)
+    end
   end
 end
--- spec/features/merchants/destroy_spec.rb
+++ spec/features/merchants/destroy_spec.rb
@@ -2,10 +2,13 @@ require 'rails_helper'
 
 RSpec.describe "As a visitor" do
   describe "When I visit a merchant show page" do
+    before(:each) do
+      @bike_shop = Merchant.create(name: "Brian's Bike Shop", address: '123 Bike Rd.', city: 'Richmond', state: 'VA', zip: 80203)
+      @chain = @bike_shop.items.create(name: "Chain", description: "It'll never break!", price: 50, image: "https://www.rei.com/media/b61d1379-ec0e-4760-9247-57ef971af0ad?size=784x588", inventory: 5)
+    end
     it "I can delete a merchant" do
-      bike_shop = Merchant.create(name: "Brian's Bike Shop", address: '123 Bike Rd.', city: 'Richmond', state: 'VA', zip: 80203)
 
-      visit "merchants/#{bike_shop.id}"
+      visit "merchants/#{@bike_shop.id}"
 
       click_on "Delete Merchant"
 
@@ -14,10 +17,7 @@ RSpec.describe "As a visitor" do
     end
 
     it "I can delete a merchant that has items" do
-      bike_shop = Merchant.create(name: "Brian's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
-      chain = bike_shop.items.create(name: "Chain", description: "It'll never break!", price: 50, image: "https://www.rei.com/media/b61d1379-ec0e-4760-9247-57ef971af0ad?size=784x588", inventory: 5)
-
-      visit "merchants/#{bike_shop.id}"
+      visit "merchants/#{@bike_shop.id}"
 
       click_on "Delete Merchant"
 
