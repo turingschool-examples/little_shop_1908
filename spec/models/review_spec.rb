a9f7856f9b6ec28fa8f8598a407a6e2b35d4dc4f
Laura Schulz <lrs8810@gmail.com>
2019-10-07 16:26:39 -0600

    Add review model spec
    Co-Authored-By: Mary Lang <mcat56@users.noreply.github.com>

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -1,1 +1,13 @@
 require 'rails_helper'
+
+RSpec.describe Review, type: :model do
+  describe "validations" do
+    it { should validate_presence_of :title }
+    it { should validate_presence_of :content }
+    it { should validate_presence_of :rating }
+  end
+
+  describe "relationships" do
+    it { should belong_to :item }
+  end
+end
