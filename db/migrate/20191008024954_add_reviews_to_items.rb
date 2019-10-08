a5de9323c84a71962f388526707a3ac30edd91e7
Brian Bower <brian@brianbower.com>
2019-10-08 10:54:20 -0600

    Continue add reviews to items

--- db/migrate/20191008024954_add_reviews_to_items.rb
+++
@@ -1,5 +0,0 @@
-class AddReviewsToItems < ActiveRecord::Migration[5.1]
-  def change
-    add_reference :items, :review, foreign_key: true
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191008024954) do
+ActiveRecord::Schema.define(version: 20191008163756) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,9 +25,7 @@ ActiveRecord::Schema.define(version: 20191008024954) do
     t.bigint "merchant_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "review_id"
     t.index ["merchant_id"], name: "index_items_on_merchant_id"
-    t.index ["review_id"], name: "index_items_on_review_id"
   end
 
   create_table "merchants", force: :cascade do |t|
@@ -44,8 +42,10 @@ ActiveRecord::Schema.define(version: 20191008024954) do
     t.string "title"
     t.string "content"
     t.integer "rating"
+    t.bigint "item_id"
+    t.index ["item_id"], name: "index_reviews_on_item_id"
   end
 
   add_foreign_key "items", "merchants"
-  add_foreign_key "items", "reviews"
+  add_foreign_key "reviews", "items"
 end
