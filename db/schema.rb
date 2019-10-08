2c78f479ff2ef1b3210e8b7542fc4e4cc36c8a04
Alice Post <alicep2322@gmail.com>
2019-10-08 14:53:43 -0600

    Start of showing reviews on Item show page
    Co-authored-by: Rachel Lew <48839191+rlew421@users.noreply.github.com>
    Co-authored-by: Zac Isaacson <50255174+zacisaacson@users.noreply.github.com>

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190817190207) do
+ActiveRecord::Schema.define(version: 20191008204443) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,14 @@ ActiveRecord::Schema.define(version: 20190817190207) do
     t.string "city"
     t.string "state"
     t.integer "zip"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.string "title"
+    t.string "content"
+    t.integer "rating"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
--- spec/features/items/show_spec.rb
+++ spec/features/items/show_spec.rb
@@ -5,8 +5,10 @@ RSpec.describe 'item show page', type: :feature do
     @bike_shop = Merchant.create(name: "Brian's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
     @chain = @bike_shop.items.create(name: "Chain", description: "It'll never break!", price: 50, image: "https://www.rei.com/media/b61d1379-ec0e-4760-9247-57ef971af0ad?size=784x588", inventory: 5)
     # make some reviews for @chain
-    # @review_1 = @chain.reviews.create(title: "This stunk", content: "super smelly", rating: 1)
-    # @review_2 = @chain.reviews.create(title: "This blew my mind", content: "goddawful", rating: 1)
+
+    @review_1 = @chain.reviews.create(title: "This stunk", content: "super smelly", rating: 1)
+    # @review_1 = Review.create(title: "This stunk", content: "super smelly", rating: 1, item: @chain.id)
+    @review_2 = @chain.reviews.create(title: "This blew my mind", content: "goddawful", rating: 1)
   end
 
   it 'shows item info' do
@@ -31,6 +33,7 @@ RSpec.describe 'item show page', type: :feature do
         expect(page).to have_content(rating.rating)
       end
     end
+  end
 
   it 'has item name link' do
     visit "/merchants/#{@bike_shop.id}/items"
