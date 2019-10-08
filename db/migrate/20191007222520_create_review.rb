db39525cceab13e7f50c8f58e7c8e06e90634bcd
Scott Regenthal <scottregenthal@gmail.com>
2019-10-07 18:00:15 -0600

    Add item reviews functionality

---
+++ db/migrate/20191007222520_create_review.rb
@@ -0,0 +1,11 @@
+class CreateReview < ActiveRecord::Migration[5.1]
+  def change
+    create_table :reviews do |t|
+      t.string :title
+      t.string :content
+      t.integer :rating
+
+      t.timestamps
+    end
+  end
+end
