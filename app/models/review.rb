63a64057f10c09678bad98ee1fbdff83a97d150f
Alice Post <alicep2322@gmail.com>
2019-10-08 15:16:21 -0600

    Model tests for Review and Item plus db migrations
    Co-authored-by: Rachel Lew <48839191+rlew421@users.noreply.github.com>
    Co-authored-by: Zac Isaacson <50255174+zacisaacson@users.noreply.github.com>

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,7 @@
 class Review < ApplicationRecord
+  belongs_to :item
 
+  validates_presence_of :title
+  validates_presence_of :content
+  validates_presence_of :rating
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191008204443) do
+ActiveRecord::Schema.define(version: 20191008211445) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,7 +44,10 @@ ActiveRecord::Schema.define(version: 20191008204443) do
     t.integer "rating"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "item_id"
+    t.index ["item_id"], name: "index_reviews_on_item_id"
   end
 
   add_foreign_key "items", "merchants"
+  add_foreign_key "reviews", "items"
 end
