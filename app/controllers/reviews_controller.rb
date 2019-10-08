00c5979a78d0200facc542027503bec60f39fef7
Graham Thompson <grwthomps@gmail.com>
2019-10-08 14:35:02 -0600

    Create edit and update methods in review controller
    Co-Authored-By: Christopher Kelling <cjkelling@users.noreply.github.com>

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -10,6 +10,18 @@ class ReviewsController <ApplicationController
     redirect_to "/items/#{item.id}"
   end
 
+  def edit
+    @item = Item.find(params[:item_id])
+    @review = Review.find(params[:review_id]) # why can't we find reviews by item?
+  end
+
+  def update
+    item = Item.find(params[:item_id])
+    review = Review.find(params[:review_id])
+    review.update(review_params)
+    redirect_to "/items/#{item.id}"
+  end
+
   private
 
   def review_params
