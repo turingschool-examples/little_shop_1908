4ad9957de4ffd52857599a335e12cca1310d93a7
Laura Schulz <lrs8810@gmail.com>
2019-10-07 19:04:16 -0600

    Add spec and implementation for flash notice on creating new reviews
    Co-Authored-By: Mary Lang <mcat56@users.noreply.github.com>

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -5,8 +5,13 @@ class ReviewsController < ApplicationController
 
   def create
     item = Item.find(params[:item_id])
-    item.reviews.create(review_params)
-    redirect_to "/items/#{params[:item_id]}"
+    review = item.reviews.new(review_params)
+    if review.save
+      redirect_to "/items/#{params[:item_id]}"
+    else
+      flash.notice = "Review not created: Required information missing."
+      redirect_to "/items/#{params[:item_id]}/reviews/new"
+    end
   end
 
   private
