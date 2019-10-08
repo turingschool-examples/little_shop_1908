6b496bcc394f991c941ba904de11185bb4e8e69e
David Lopez <ldavid.x.lopezl@gmail.com>
2019-10-08 16:15:37 -0600

    Best practice testing
    Co-Authored-By: Hillary <hillstew@users.noreply.github.com>

--- spec/features/items/edit_spec.rb
+++ spec/features/items/edit_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe "As a Visitor" do
   describe "When I visit an Item Show Page" do
     describe "and click on edit item" do
-      it 'I can see the prepopulated fields of that item' do
+      before(:each) do
         @meg = Merchant.create(name: "Meg's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
         @tire = @meg.items.create(name: "Gatorskins", description: "They'll never pop!", price: 100, image: "https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588", inventory: 12)
 
@@ -12,7 +12,8 @@ RSpec.describe "As a Visitor" do
         expect(page).to have_link("Edit Item")
 
         click_on "Edit Item"
-
+      end
+      it 'I can see the prepopulated fields of that item' do
         expect(current_path).to eq("/items/#{@tire.id}/edit")
         expect(page).to have_link("Gatorskins")
         expect(find_field('Name').value).to eq "Gatorskins"
@@ -23,13 +24,6 @@ RSpec.describe "As a Visitor" do
       end
 
       it 'I can change and update item with the form' do
-        @meg = Merchant.create(name: "Meg's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
-        @tire = @meg.items.create(name: "Gatorskins", description: "They'll never pop!", price: 100, image: "https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588", inventory: 12)
-
-        visit "/items/#{@tire.id}"
-
-        click_on "Edit Item"
-
         fill_in 'Name', with: "GatorSkins"
         fill_in 'Price', with: 110
         fill_in 'Description', with: "They're a bit more expensive, and they kinda do pop sometimes, but whatevs.. this is retail."
