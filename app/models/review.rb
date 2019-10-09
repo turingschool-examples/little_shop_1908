1ead0429e9e7a9c7fb87466198fae00b334bc368
Scott Regenthal <scottregenthal@gmail.com>
2019-10-09 14:21:32 -0600

    Include top and bottom 3 reviews and average item rating on show page
    Co-authored-by: Ryan Hantak <47759923+rhantak@users.noreply.github.com>

--- app/models/review.rb
+++ app/models/review.rb
@@ -8,4 +8,17 @@ class Review < ApplicationRecord
   validates_numericality_of :rating,  only_integer: true,
                                       less_than_or_equal_to: 5,
                                       greater_than_or_equal_to: 1
+
+  def self.top
+    Review.order(:rating).limit(3)
+  end
+
+  def self.bottom
+    Review.order(rating: :desc).limit(3)
+  end
+
+  def self.avg_rating
+    Review.average(:rating).round(2)
+  end
+
 end
