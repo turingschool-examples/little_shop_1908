e9fe4b093fbfbeeb58ccb95bd7ff2a3e05356c67
Alice Post <alicep2322@gmail.com>
2019-10-09 16:33:32 -0600

    Add incomplete edit review flash message
    Co-authored-by: Rachel Lew <48839191+rlew421@users.noreply.github.com>
    Co-authored-by: Zac Isaacson <50255174+zacisaacson@users.noreply.github.com>

--- spec/features/reviews/edit_spec.rb
+++ spec/features/reviews/edit_spec.rb
@@ -1,17 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe "Edit a Review" do
+  before(:each) do
+    @bike_shop = Merchant.create(name: "Brian's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
+    @chain = @bike_shop.items.create(name: "Chain", description: "It'll never break!", price: 50, image: "https://www.rei.com/media/b61d1379-ec0e-4760-9247-57ef971af0ad?size=784x588", inventory: 5)
+
+    @review_1 = @chain.reviews.create(title: "This stunk", content: "super smelly", rating: 1)
+  end
+
   describe "On an item's show page" do
-    before(:each) do
-      @bike_shop = Merchant.create(name: "Brian's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
-      @chain = @bike_shop.items.create(name: "Chain", description: "It'll never break!", price: 50, image: "https://www.rei.com/media/b61d1379-ec0e-4760-9247-57ef971af0ad?size=784x588", inventory: 5)
-
-      @review_1 = @chain.reviews.create(title: "This stunk", content: "super smelly", rating: 1)
-    end
-
     it "I can click a link to edit a review via form" do
       visit "/items/#{@chain.id}"
-      
+
       click_link "Edit Review"
 
       expect(current_path).to eq("/items/#{@chain.id}/reviews/#{@review_1.id}/edit")
@@ -26,10 +26,23 @@ RSpec.describe "Edit a Review" do
       expect(page).to have_content("Amaaaazing")
       expect(page).to have_content("Rating: 5")
       expect(page).to have_content("The best chain in the history of chains")
-
     end
   end
 
+  describe "with incomplete review edit form" do
+    it "shows a flash message" do
+      visit "/items/#{@chain.id}/reviews/#{@review_1.id}/edit"
+
+      fill_in :title, with: "Cool chain dude."
+      fill_in :content, with: "Off the chain."
+
+      click_button "Update Review"
+
+      expect(page).to have_content("Review not updated. Please fill in all fields.")
+      expect(page).to have_button("Update Review")
+
+    end
+  end
 end
 
 
