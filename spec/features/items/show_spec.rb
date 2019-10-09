c7a2fa2cf09bf1e4f9614f3be50769155938546d
David Gomez <davdg25@gmail.com>
2019-10-09 11:36:22 -0600

    Finishied display of reviews on to page
    Co-authored-by: Jonathan Patterson <47950816+Jonpatt92@users.noreply.github.com>

--- spec/features/items/show_spec.rb
+++ spec/features/items/show_spec.rb
@@ -7,8 +7,9 @@ RSpec.describe 'item show page', type: :feature do
       @chain = @bike_shop.items.create(name: "Chain", description: "It'll never break!", price: 50, image: "https://www.rei.com/media/b61d1379-ec0e-4760-9247-57ef971af0ad?size=784x588", inventory: 5)
 
       @review_1 = @chain.reviews.create(title: 'Leiya', content: 'Awful chain, Meg lied to me!', rating: 1)
-      @review_2 = @chain.reviews.create(title: 'Josh', content: " It wasn't that bad.", rating: 3)
-      @review_3 = @chain.reviews.create(title: 'Evette', content: " Great chain! Used it to make an amazing collar for my pug Larry.", rating: 5)
+      @review_2 = @chain.reviews.create(title: 'Josh', content: "It wasn't that bad.", rating: 3)
+      @review_3 = @chain.reviews.create(title: 'Evette', content: "Great chain! Used it to make an amazing collar for my pug Larry.", rating: 5)
+
     end
 
     it 'shows item info' do
@@ -26,6 +27,7 @@ RSpec.describe 'item show page', type: :feature do
 
     it 'shows a list of reviews with title, content, and rating' do
       visit "items/#{@chain.id}"
+      # save_and_open_page
 
       expect(page).to have_content(@review_1.title)
       expect(page).to have_content(@review_2.title)
