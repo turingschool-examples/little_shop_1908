338f5c6d2fa939d07682b8d4b4835bb1185a7f74
David Lopez <ldavid.x.lopezl@gmail.com>
2019-10-09 14:33:21 -0600

    Update validations in review
    Co-Authored-By: Hillary <hillstew@users.noreply.github.com>

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -4,7 +4,7 @@ describe Review, type: :model do
   describe 'validations' do
     it { should validate_presence_of :title }
     it { should validate_presence_of :content }
-    it { should validate_presence_of :rating }
+    it { should validate_inclusion_of(:rating).in_range(1..5) }
   end
 
   describe 'relationships' do
