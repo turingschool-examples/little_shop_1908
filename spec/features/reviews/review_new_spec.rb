16dbe3b548981485e22058c1516b9673598a3e3a
Tyla Devon <tyladevon@gmail.com>
2019-10-09 12:31:42 -0600

    Finish create_review and tests

--- spec/features/reviews/review_new_spec.rb
+++ spec/features/reviews/review_new_spec.rb
@@ -5,17 +5,15 @@ describe "Review page", type: :feature do
 
     @bike_shop = Merchant.create!(name: "Brian's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
     @chain = @bike_shop.items.create!(name: "Chain", description: "It'll never break!", price: 50, image: "https://www.rei.com/media/b61d1379-ec0e-4760-9247-57ef971af0ad?size=784x588", inventory: 5)
-    @review_1 = @chain.reviews.create!(title: "Best Item Ever", content: "I mean it. This item is the best thing ever!", rating: 5)
-    @review_2 = @chain.reviews.create!(title: "Waste Of Money", content: "What a total waste of money. This item broke on the first use.", rating: 1)
 
     visit "/items/#{@chain.id}"
-
     click_link "Write Review"
+    save_and_open_page
     expect(current_path).to eq("/items/#{@chain.id}/reviews/new")
 
-    fill_in "Title", with: "Best Item Ever"
-    fill_in "Content", with: "I mean it. This item is the best thing ever!"
-    fill_in "Rating", with: 5
+    fill_in :title, with: "Best Item Ever"
+    fill_in :content, with: "I mean it. This item is the best thing ever!"
+    fill_in :rating, with: 5
 
     click_button "Submit Review"
     expect(current_path).to eq("/items/#{@chain.id}")
