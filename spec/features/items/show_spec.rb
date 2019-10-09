0ecf4f4a61cb1227572a9ea94c1536883be8b1e6
David Lopez <ldavid.x.lopezl@gmail.com>
2019-10-09 09:22:25 -0600

    Rid of factory bot and fix destroy test

--- spec/features/items/show_spec.rb
+++ spec/features/items/show_spec.rb
@@ -2,10 +2,13 @@ require 'rails_helper'
 
 RSpec.describe 'item show page', type: :feature do
   before(:each) do
-    @review_1 = create(:chain_review_1)
-    @review_2 = create(:chain_review_2)
+    @bike_shop = Merchant.create(name: "Brian's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
+    @chain = @bike_shop.items.create(name: "Chain", description: "It'll never break!", price: 50, image: "https://www.rei.com/media/b61d1379-ec0e-4760-9247-57ef971af0ad?size=784x588", inventory: 5)
+    @review_1 = @chain.reviews.create(title: "Best Chain!", content: "It never broke!", rating: 5)
+    @review_2 = @chain.reviews.create(title: "Wurst Chain", content: "It broke :(", rating: 1)
     visit "items/#{@review_1.item.id}"
   end
+
   it 'shows item info' do
     within("#item-show-#{@review_1.item.id}") do
       expect(page).to have_link(@review_1.item.merchant.name)
@@ -20,14 +23,14 @@ RSpec.describe 'item show page', type: :feature do
   end
   it 'shows all item reviews' do
     within("#item-review-#{@review_1.id}") do
-      expect(page).to have_content("Best Chain evur")
+      expect(page).to have_content("Best Chain!")
       expect(page).to have_content("It never broke!")
       expect(page).to have_content('5')
     end
 
     within("#item-review-#{@review_2.id}") do
-      expect(page).to have_content("Wurst Chain evur")
-      expect(page).to have_content("It broke!")
+      expect(page).to have_content("Wurst Chain")
+      expect(page).to have_content("It broke :(")
       expect(page).to have_content('1')
     end
   end
--- spec/features/merchants/destroy_spec.rb
+++ spec/features/merchants/destroy_spec.rb
@@ -1,18 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe "As a visitor" do
+  before :each do
+    @bike_shop = Merchant.create(name: "Brian's Bike Shop", address: '123 Bike Rd.', city: 'Richmond', state: 'VA', zip: 11234)
+  end
+
   describe "When I visit a merchant show page" do
     it "I can delete a merchant" do
-      bike_shop = create(:brians_bike_shop)
-
-      visit "merchants/#{bike_shop.id}"
+      visit "merchants/#{@bike_shop.id}"
       click_on "Delete Merchant"
       expect(current_path).to eq('/merchants')
       expect(page).to_not have_content("Brian's Bike Shop")
     end
 
     it "I can delete a merchant that has items" do
-      item = create(:chain)
+      item = @bike_shop.items.create(name: "Chain", description: "It'll never break!", price: 50, image: "https://www.rei.com/media/b61d1379-ec0e-4760-9247-57ef971af0ad?size=784x588", inventory: 5)
       visit "/merchants/#{item.merchant_id}"
 
       click_on "Delete Merchant"
