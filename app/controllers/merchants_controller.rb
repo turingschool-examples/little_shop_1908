483997047f30a95f4b09282440bd9ce5607e4ed2
John Travers <johnk.travers@gmail.com>
2019-10-09 15:31:02 -0600

    Add flash messages for sad paths for merchant create, edit, and show
    Co-authored-by: Michael Cooper <8922930+CoopTang@users.noreply.github.com>

--- app/controllers/merchants_controller.rb
+++ app/controllers/merchants_controller.rb
@@ -5,15 +5,27 @@ class MerchantsController < ApplicationController
   end
 
   def show
-    @merchant = Merchant.find(params[:id])
+    merchant = Merchant.where(id: params[:id])
+    if !merchant.empty?
+      @merchant = merchant.first
+    else
+      flash[:error] = ['Merchant does not exist']
+      redirect_to '/merchants'
+    end
   end
 
   def new
   end
 
   def create
-    Merchant.create(merchant_params)
-    redirect_to "/merchants"
+    merchant = Merchant.new(merchant_params)
+
+    if merchant.save
+      redirect_to '/merchants'
+    else
+      flash[:error] = merchant.errors.full_messages
+      redirect_to '/merchants/new'
+    end
   end
 
   def edit
@@ -22,8 +34,12 @@ class MerchantsController < ApplicationController
 
   def update
     merchant = Merchant.find(params[:id])
-    merchant.update(merchant_params)
-    redirect_to "/merchants/#{merchant.id}"
+    if merchant.update(merchant_params)
+      redirect_to "/merchants/#{merchant.id}"
+    else
+      flash[:error] = merchant.errors.full_messages
+      redirect_to "/merchants/#{merchant.id}/edit"
+    end
   end
 
   def destroy
--- spec/features/merchants/new_spec.rb
+++ spec/features/merchants/new_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe 'merchant new page', type: :feature do
 
-  it 'I can create a new merchant' do
+  it 'can create a new merchant' do
     visit '/merchants/new'
 
     name = "Sal's Calz(ones)"
@@ -17,7 +17,7 @@ RSpec.describe 'merchant new page', type: :feature do
     fill_in :state, with: state
     fill_in :zip, with: zip
 
-    click_button "Create Merchant"
+    click_button 'Create Merchant'
 
     new_merchant = Merchant.last
 
@@ -28,6 +28,33 @@ RSpec.describe 'merchant new page', type: :feature do
     expect(new_merchant.city).to eq(city)
     expect(new_merchant.state).to eq(state)
     expect(new_merchant.zip).to eq(zip)
+  end
+
+  it 'flashes a message and redirects if form is not filled in' do
+    visit '/merchants/new'
+    click_button 'Create Merchant'
+
+    expect(page).to have_content("Name can't be blank")
+    expect(page).to have_content("Address can't be blank")
+    expect(page).to have_content("City can't be blank")
+    expect(page).to have_content("State can't be blank")
+    expect(page).to have_content("Zip can't be blank")
+
+    expect(current_path).to eq('/merchants/new')
+  end
+
+  it 'flashes a message and redirects if zip code has wrong format' do
+    visit '/merchants/new'
+
+    fill_in :zip, with: '2461'
+    click_button 'Create Merchant'
+    expect(page).to have_content('Zip is the wrong length (should be 5 characters)')
+    expect(current_path).to eq('/merchants/new')
+
+    fill_in :zip, with: '24611.2'
+    click_button 'Create Merchant'
+    expect(page).to have_content('Zip must be an integer')
+    expect(current_path).to eq('/merchants/new')
   end
 
 end
