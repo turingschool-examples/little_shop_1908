1f53bf4a71331c1c8d5c4686b1ebd62897989efa
Hillary Stewart <hillarystewart4427@gmail.com>
2019-10-09 16:51:31 -0600

    Add additional seed data
    Co-Authored-By: David Lopez <Not-Zorro@users.noreply.github.com>

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,7 +15,18 @@ dog_shop = Merchant.create(name: "Brian's Dog Shop", address: '125 Doggo St.', c
 
 #bike_shop items
 tire = bike_shop.items.create(name: "Gatorskins", description: "They'll never pop!", price: 100, image: "https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588", inventory: 12)
+chain = bike_shop.items.create(name: "Chain", description: "It'll never break!", price: 50, image: "https://www.rei.com/media/b61d1379-ec0e-4760-9247-57ef971af0ad?size=784x588", inventory: 5)
 
 #dog_shop items
 pull_toy = dog_shop.items.create(name: "Pull Toy", description: "Great pull toy!", price: 10, image: "http://lovencaretoys.com/image/cache/dog/tug-toy-dog-pull-9010_2-800x800.jpg", inventory: 32)
 dog_bone = dog_shop.items.create(name: "Dog Bone", description: "They'll love it!", price: 21, image: "https://img.chewy.com/is/image/catalog/54226_MAIN._AC_SL1500_V1534449573_.jpg", active?:false, inventory: 21)
+
+chain.reviews.create(title: "Best Chain!", content: "It never broke!", rating: 5)
+chain.reviews.create(title: "Wurst Chain", content: "It broke :(", rating: 5)
+chain.reviews.create(title: "Meh place", content: "They have meh bike stuff and I probably won't come back", rating: 5)
+chain.reviews.create(title: "Not too impressed", content: "v basic bike shop", rating: 2)
+dog_bone.reviews.create(title: "Okay place :/", content: "Brian's cool and all but just an okay selection of items", rating: 3)
+dog_bone.reviews.create(title: "COOL :/", content: "sometimes questionable", rating: 1)
+dog_bone.reviews.create(title: "NEAT :/", content: "lets go", rating: 1)
+tire.reviews.create(title: "SUPER :/", content: "not that great", rating: 1)
+tire.reviews.create(title: "NICE :/", content: "cool cool", rating: 3)
