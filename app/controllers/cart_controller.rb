27d376ff09aa01dacec0ec9df93a08d4cc662cc9
Jon Patterson <JonPatt92@gmail.com>
2019-10-09 14:43:55 -0600

    Build update for cart controller

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -1,4 +1,13 @@
 class CartController < ApplicationController
+  include ActionView::Helpers::TextHelper
+
   def update
+    item = Item.find(params[:item_id])
+    @cart = Cart.new(session[:cart])
+    @cart.add_item(item.id)
+    session[:cart] = @cart.contents
+    quantity = @cart.count_of(item.id)
+    flash[:notice] = "You now have #{pluralize(quantity, "copy")} of #{item.title} in your cart."
+    redirect_to items_path
   end
 end
