3a837d4b0258b60783830806c635e0da57c250da
Hillary Stewart <hillarystewart4427@gmail.com>
2019-10-10 17:54:59 -0600

    Add test for emptying cart
    Co-Authored-By: David Lopez <Not-Zorro@users.noreply.github.com>

--- spec/features/cart/cart_show_spec.rb
+++ spec/features/cart/cart_show_spec.rb
@@ -1,18 +1,6 @@
 require 'rails_helper'
 
-describe "Cart Show Page" do
-  #   As a visitor
-  # When I have added items to my cart
-  # And I visit my cart ("/cart")
-  # I see all items I've added to my cart
-  # Each item in my cart shows the following information:
-  # - the name of the item
-  # - the item image
-  # - the merchant I'm buying this item from
-  # - the price of the item
-  # - my desired quantity of the item
-  # - a subtotal (price multiplied by quantity)
-  # I also see a grand total of what everything in my cart will cost
+describe "Cart Index" do
   it "can list all items and info" do
     meg = Merchant.create(name: "Meg's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
     brian = Merchant.create(name: "Brian's Dog Shop", address: '125 Doggo St.', city: 'Denver', state: 'CO', zip: 80210)
@@ -60,6 +48,35 @@ describe "Cart Show Page" do
 
     within ".cart-section" do
       expect(page).to have_content("Grand total: $50")
+    end
+  end
+
+  it "has a link to empty cart" do
+    brian = Merchant.create(name: "Brian's Dog Shop", address: '125 Doggo St.', city: 'Denver', state: 'CO', zip: 80210)
+    pull_toy = brian.items.create(name: "Pull Toy", description: "Great pull toy!", price: 10, image: "http://lovencaretoys.com/image/cache/dog/tug-toy-dog-pull-9010_2-800x800.jpg", inventory: 32)
+    dog_bone = brian.items.create(name: "Dog Bone", description: "They'll love it!", price: 20, image: "https://img.chewy.com/is/image/catalog/54226_MAIN._AC_SL1500_V1534449573_.jpg", inventory: 21)
+    
+    visit '/items'
+
+    within "#item-#{pull_toy.id}" do
+      click_button 'Add to cart'
+    end
+
+    within "#item-#{dog_bone.id}" do
+      click_button 'Add to cart'
+      click_button 'Add to cart'
+    end
+
+    visit '/cart'
+
+    within (".cart_div") do
+      expect(page).to have_content("3")
+    end
+
+    click_link 'Empty Cart'
+
+    within (".cart_div") do
+      expect(page).to have_content("0")
     end
   end
 end
