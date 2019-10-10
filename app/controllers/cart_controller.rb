c49c696dc9ea8e116d992ea0c7547035c216159c
Jon Patterson <JonPatt92@gmail.com>
2019-10-10 16:00:37 -0600

    Tweak cart show view, add show method to cart controller, tests passing, story 12 done

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -1,12 +1,16 @@
 class CartController < ApplicationController
   include ActionView::Helpers::TextHelper
 
+  def show
+    @cart = cart
+  end
+
   def update
     item = Item.find(params[:item_id])
     cart.add_item(item.id)
     session[:cart] = cart.contents
     quantity = cart.contents[item.id.to_s]
-    binding.pry
+
     flash[:confirm] = "#{item.name} added to cart"
     flash[:notice] = "You now have #{pluralize(quantity, "#{item.name}")} in your cart."
     redirect_to items_path
