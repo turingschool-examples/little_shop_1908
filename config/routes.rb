faaaca8eafa0a8fdc9532cc9992e058f16c2a7ca
Jon Patterson <JonPatt92@gmail.com>
2019-10-10 15:52:29 -0600

    Add route for cart show page

--- config/routes.rb
+++ config/routes.rb
@@ -21,5 +21,6 @@ Rails.application.routes.draw do
   get "/items/:item_id/reviews/new", to: "reviews#new"
   post "/items/:item_id", to: "reviews#create"
 
+  get "/cart", to: "cart#show"
   patch "/cart/:item_id", to: "cart#update"
 end
