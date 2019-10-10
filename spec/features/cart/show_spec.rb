c427ca7484041fdfae842b01f32be5504ae4a2cb
Michael Cooper <macoop2363@gmail.com>
2019-10-10 14:27:22 -0600

    Add test for cart show page
    Co-authored-by: John Travers <46035439+johnktravers@users.noreply.github.com>

---
+++ spec/features/cart/show_spec.rb
@@ -0,0 +1,59 @@
+require 'rails_helper'
+
+RSpec.describe 'cart show page' do
+  describe 'when I visit my cart' do 
+    before(:each) do
+      @meg = Merchant.create(name: "Meg's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
+      @brian = Merchant.create(name: "Brian's Dog Shop", address: '125 Doggo St.', city: 'Denver', state: 'CO', zip: 80210)
+  
+      @tire = @meg.items.create(name: "Gatorskins", description: "They'll never pop!", price: 100, image: "https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588", inventory: 12)
+  
+      @pull_toy = @brian.items.create(name: "Pull Toy", description: "Great pull toy!", price: 10, image: "http://lovencaretoys.com/image/cache/dog/tug-toy-dog-pull-9010_2-800x800.jpg", inventory: 32)
+      @dog_bone = @brian.items.create(name: "Dog Bone", description: "They'll love it!", price: 21, image: "https://img.chewy.com/is/image/catalog/54226_MAIN._AC_SL1500_V1534449573_.jpg", active?:false, inventory: 21)
+
+      visit "/items/#{@tire.id}"
+      click_button 'Add Item to Cart'
+
+      visit "/items/#{@pull_toy.id}"
+      click_button 'Add Item to Cart'
+
+      visit "/items/#{@dog_bone.id}"
+      click_button 'Add Item to Cart'
+
+      visit "/items/#{@tire.id}"
+      click_button 'Add Item to Cart'
+
+      visit '/cart'
+    end
+
+    it 'should show all products and product info' do
+      within "#item-#{@tire.id}" do
+        expect(page).to have_content(@tire.name)
+        expect(page).to have_css("img[src=#{@tire.image}]")
+        expect(page).to have_content(@tire.merchant.name)
+        expect(page).to have_content("Price: #{@tire.price}")
+        expect(page).to have_content("Quantity: 2")
+        expect(page).to have_content("Subtotal: 200")
+      end
+
+      within "#item-#{@pull_toy.id}" do
+        expect(page).to have_content(@pull_toy.name)
+        expect(page).to have_css("img[src=#{@pull_toy.image}]")
+        expect(page).to have_content(@pull_toy.merchant.name)
+        expect(page).to have_content("Price: #{@pull_toy.price}")
+        expect(page).to have_content("Quantity: 1")
+        expect(page).to have_content("Subtotal: 10")
+      end
+
+      within "#item-#{@dog_bone.id}" do
+        expect(page).to have_content(@dog_bone.name)
+        expect(page).to have_css("img[src=#{@dog_bone.image}]")
+        expect(page).to have_content(@dog_bone.merchant.name)
+        expect(page).to have_content("Price: #{@dog_bone.price}")
+        expect(page).to have_content("Quantity: 1")
+        expect(page).to have_content("Subtotal: 21")
+      end
+      
+    end
+  end
+end
