3d4cf5482fbd6b56ecf00ddcc91f8144bf67b77d
Graham Thompson <grwthomps@gmail.com>
2019-10-11 14:14:22 -0600

    Refactor remove item cart route
    Co-Authored-By: Christopher Kelling <cjkelling@users.noreply.github.com>

--- config/routes.rb
+++ config/routes.rb
@@ -25,6 +25,6 @@ Rails.application.routes.draw do
 
   get '/cart', to: "cart#show"
   patch '/cart/:item_id', to: "cart#update"
-  patch '/cart', to: "cart#remove_item"
+  patch '/cart/remove_item/:item_id', to: "cart#remove_item"
   delete '/cart', to: "cart#destroy"
 end
