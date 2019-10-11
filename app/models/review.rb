089a96604d93a497fbeb1f9bfb978096a1d65519
Ryan Hantak <rhantak@gmail.com>
2019-10-10 22:40:34 -0600

    Rewrite review stats to separate best and worst reviews
    Co-authored-by: Scott Regenthal <50811220+freeheeling@users.noreply.github.com>

--- app/models/review.rb
+++ app/models/review.rb
@@ -9,17 +9,16 @@ class Review < ApplicationRecord
                                       less_than_or_equal_to: 5,
                                       greater_than_or_equal_to: 1
 
-  def self.best_and_worst
-    if Review.all.count > 6
-      reviews_shown = Review.order(:rating).limit(3) + Review.order(rating: :desc).limit(3)
-    else
-      reviews_shown = Review.all
-    end
-    reviews_shown
-  end
-
   def self.avg_rating
     Review.average(:rating).round(2)
+  end
+
+  def self.bottom
+    Review.order(:rating).limit(3)
+  end
+
+  def self.top
+    Review.order(rating: :desc).limit(3)
   end
 
 end
