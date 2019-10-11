0fa7e000794b31f0b25b4f4c41040845cbba590b
Rachel Lew <rlew07@gmail.com>
2019-10-11 17:18:18 -0600

    Add model test to test it can calculate item subtotal by item id
    Co-authored-by: Alice Post <26877629+ap2322@users.noreply.github.com>
    Co-authored-by: Zac Isaacson  <50255174+zacisaacson@users.noreply.github.com>

--- spec/models/cart_spec.rb
+++ spec/models/cart_spec.rb
@@ -33,5 +33,16 @@ RSpec.describe Cart do
       cart.add_item("2")
       expect(cart.count_of("2")).to eq(4)
     end
+
+    it "#subtotal can calculate the subtotal of item by its id" do
+      meg = Merchant.create(name: "Meg's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
+      chain = meg.items.create(name: "Chain", description: "It'll never break!", price: 50, image: "https://www.rei.com/media/b61d1379-ec0e-4760-9247-57ef971af0ad?size=784x588", inventory: 5)
+      shifter = meg.items.create(name: "Shimano Shifters", description: "It'll always shift!", active?: false, price: 180, image: "https://images-na.ssl-images-amazon.com/images/I/4142WWbN64L._SX466_.jpg", inventory: 2)
+
+      cart = Cart.new({chain.id => 1, shifter.id => 1})
+      expect(cart.subtotal(chain.id)).to eq(50)
+      cart.add_item(chain.id)
+      expect(cart.subtotal(chain.id)).to eq(100)
+    end
   end
 end
