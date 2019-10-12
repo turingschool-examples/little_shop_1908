01c2cc674962e72f8c348454bdf27f1100c1d538
Ryan Hantak <rhantak@gmail.com>
2019-10-12 16:00:56 -0600

    Restructure item increment route to use patch verb
    Co-authored-by: Scott Regenthal <50811220+freeheeling@users.noreply.github.com>

--- config/routes.rb
+++ config/routes.rb
@@ -28,7 +28,7 @@ Rails.application.routes.draw do
 
   get '/cart', to: 'cart#show'
   patch '/cart/:item_id', to: 'cart#update'
-  put '/cart/:item_id', to: 'cart#add'
+  patch '/cart/:item_id/add', to: 'cart#add'
   delete '/cart/:item_id', to: 'cart#remove'
   delete '/cart', to: 'cart#destroy'
 end
