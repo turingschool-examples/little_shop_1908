3e8dfbfcd865e186a932ad4a453848b48412173c
Michael Cooper <macoop2363@gmail.com>
2019-10-12 14:19:30 -0600

    Add show and create functionality for orders
    Co-authored-by: John Travers <46035439+johnktravers@users.noreply.github.com>

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,4 +4,22 @@ class OrdersController < ApplicationController
     @items = Item.find(cart.contents.keys)
   end
 
+  def show
+    @item_orders = ItemOrder.where(order_id: params[:id])
+  end
+
+  def create
+    user = User.create(user_params)
+    order = user.orders.create(grand_total: cart.grand_total)
+
+    cart.contents.each do |item_id, quantity|
+      order.item_orders.create(item_id: item_id, item_quantity: quantity, subtotal: cart.subtotal(item_id))
+    end
+    redirect_to "/orders/#{order.id}"
+  end
+
+  private
+    def user_params
+      params.permit(:name, :address, :city, :state, :zip)
+    end
 end
