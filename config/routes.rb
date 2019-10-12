7344917959a41477e50edd8dccdb3fb422ecbf54
David Lopez <ldavid.x.lopezl@gmail.com>
2019-10-12 10:11:56 -0600

    Add route for delete item
    Co-Authored-By: Hillary <hillstew@users.noreply.github.com>

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/cart', to: 'cart#index'
   delete '/cart/empty', to: 'cart#empty'
+  delete '/cart/:item_id', to: 'cart#delete_item'
   get "/merchants", to: "merchants#index"
   get "/merchants/new", to: "merchants#new"
   get "/merchants/:id", to: "merchants#show"
