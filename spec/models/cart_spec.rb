b47454ffc70b2d44b0456e730283094b43262484
Zac Isaacson <isaacson.zm@gmail.com>
2019-10-12 12:14:58 -0600

    Adds empty cart functionality
    Co-authored-by: Rachel Lew <48839191+rlew421@users.noreply.github.com>
    Co-authored-by: Alice Post <26877629+ap2322@users.noreply.github.com>

--- spec/models/cart_spec.rb
+++ spec/models/cart_spec.rb
@@ -58,5 +58,16 @@ RSpec.describe Cart do
       cart.add_item(chain.id)
       expect(cart.grand_total).to eq(280)
     end
+
+    it "#empty_cart can remove all items from cart" do
+      meg = Merchant.create(name: "Meg's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
+      chain = meg.items.create(name: "Chain", description: "It'll never break!", price: 50, image: "https://www.rei.com/media/b61d1379-ec0e-4760-9247-57ef971af0ad?size=784x588", inventory: 5)
+      shifter = meg.items.create(name: "Shimano Shifters", description: "It'll always shift!", active?: false, price: 180, image: "https://images-na.ssl-images-amazon.com/images/I/4142WWbN64L._SX466_.jpg", inventory: 2)
+      cart = Cart.new({chain.id.to_s => 1, shifter.id.to_s => 1})
+
+      empty = {}
+      cart.empty_cart
+      expect(cart.contents).to eq(empty)
+    end
   end
 end
