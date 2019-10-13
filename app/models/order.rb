e4215d0d5afa70b25231a7afd99fe3380f09d381
Scott Regenthal <scottregenthal@gmail.com>
2019-10-13 15:37:45 -0600

    Create model for order resources
    Co-authored-by: Ryan Hantak <47759923+rhantak@users.noreply.github.com>

--- app/models/order.rb
+++ app/models/order.rb
@@ -8,4 +8,14 @@ class Order < ApplicationRecord
                         :state,
                         :zip,
                         :grand_total
+
+  def generate_item_orders(cart)
+    cart.cart_items.each do |item|
+      ItemOrder.create( order_id: id,
+                        item_id: item.id,
+                        quantity: cart.count_of(item.id),
+                        subtotal: (cart.count_of(item.id) * item.price)
+                      )
+    end
+  end
 end
