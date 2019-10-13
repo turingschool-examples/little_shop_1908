0aaea793dcbd24ff13b554699dcc11cd563b049c
Ryan Hantak <rhantak@gmail.com>
2019-10-13 10:42:14 -0600

    Add functionality to item#destroy to allow for deletion of items with reviews
    Co-authored-by: Scott Regenthal <50811220+freeheeling@users.noreply.github.com>

--- spec/features/items/destroy_spec.rb
+++ spec/features/items/destroy_spec.rb
@@ -2,13 +2,26 @@ require 'rails_helper'
 
 RSpec.describe 'item delete', type: :feature do
   describe 'when I visit an item show page' do
-    it 'I can delete an item' do
+    it 'I can click a link to delete an item' do
       bike_shop = Merchant.create(name: "Brian's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
       chain = bike_shop.items.create(name: "Chain", description: "It'll never break!", price: 50, image: "https://www.rei.com/media/b61d1379-ec0e-4760-9247-57ef971af0ad?size=784x588", inventory: 5)
 
       visit "/items/#{chain.id}"
 
       expect(page).to have_link("Delete Item")
+
+      click_on "Delete Item"
+
+      expect(current_path).to eq("/items")
+      expect(page).to_not have_css("#item-#{chain.id}")
+    end
+
+    it "Clicking the link will delete an item even if it has reviews" do
+      bike_shop = Merchant.create(name: "Brian's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
+      chain = bike_shop.items.create(name: "Chain", description: "It'll never break!", price: 50, image: "https://www.rei.com/media/b61d1379-ec0e-4760-9247-57ef971af0ad?size=784x588", inventory: 5)
+      review = chain.reviews.create(title: "I love it", content: "Totally fixed my bike issues.", rating: 5)
+
+      visit "/items/#{chain.id}"
 
       click_on "Delete Item"
 
