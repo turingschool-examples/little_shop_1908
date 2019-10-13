e1a8577e2d3b26d4665dbbebdefd9ad3e1f7ef45
Christopher Kelling <cj.kelling88@gmail.com>
2019-10-13 10:02:16 -0600

    Created new view to + - in cart. Also create add and subtract methods in model.

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -9,7 +9,7 @@ class Cart
   end
 
   def contents
-    @contents.map do |item_id, number_in_cart|
+    @contents.map do |item_id, _number_in_cart|
       Item.find(item_id)
     end
   end
@@ -19,14 +19,26 @@ class Cart
   end
 
   def subtotal(item_id)
-    (Item.find(item_id).price) * @contents[item_id.to_s]
+    Item.find(item_id).price * @contents[item_id.to_s]
   end
 
   def grand_total
     total = 0
-    @contents.each do |k, v|
+    @contents.each do |k, _v|
       total += subtotal(k)
     end
     total
+  end
+
+  def add_item(item_id)
+    @contents[item_id] += 1
+  end
+
+  def subtract_item(item_id)
+    @contents[item_id] -= 1
+  end
+
+  def inventory_limit?(item_id)
+    item_count(item_id) == Item.find(item_id).inventory
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -25,4 +25,7 @@ Rails.application.routes.draw do
 
   get '/cart', to: "cart#show"
   patch '/cart/:item_id', to: "cart#update"
+  patch '/cart/remove_item/:item_id', to: "cart#remove_item"
+  delete '/cart', to: "cart#destroy"
+  patch '/cart/:add_subtract/:item_id', to: 'cart#add_subtract'
 end
