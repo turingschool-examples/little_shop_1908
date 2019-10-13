ce0ade55f6b51d369a6e701d03e95373650a7523
Michael Cooper <macoop2363@gmail.com>
2019-10-13 14:36:42 -0600

    Add test to display total number of items and average item price for a merchant

--- spec/features/merchants/show_spec.rb
+++ spec/features/merchants/show_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe 'merchant show page', type: :feature do
 
   before :each do
     @bike_shop = Merchant.create(name: "Brian's Bike Shop", address: '123 Bike Rd.', city: 'Richmond', state: 'VA', zip: '23137')
+    @chain = @bike_shop.items.create(name: "Chain", description: "It'll never break!", price: 50, image: "https://www.rei.com/media/b61d1379-ec0e-4760-9247-57ef971af0ad?size=784x588", inventory: 5)
   end
 
   it 'can see a merchants name, address, city, state, zip' do
@@ -27,4 +28,24 @@ RSpec.describe 'merchant show page', type: :feature do
     expect(current_path).to eq('/merchants')
   end
 
+  it 'can see the total number of items a merchant has' do
+    visit "/merchants/#{@bike_shop.id}"
+    
+    expect(page).to have_content("Total Items: 1")
+
+    chain = @bike_shop.items.create(name: "Chain", description: "It'll never break!", price: 50, image: "https://www.rei.com/media/b61d1379-ec0e-4760-9247-57ef971af0ad?size=784x588", inventory: 5)
+
+    visit "/merchants/#{@bike_shop.id}"
+
+    expect(page).to have_content("Total Items: 2")
+  end
+
+  it 'can see the average price of all their items' do
+    chain = @bike_shop.items.create(name: "Chain", description: "It'll never break!", price: 50, image: "https://www.rei.com/media/b61d1379-ec0e-4760-9247-57ef971af0ad?size=784x588", inventory: 5)
+    @tire = @bike_shop.items.create(name: "Gatorskins", description: "They'll never pop!", price: 100, image: "https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588", inventory: 12)
+
+    visit "/merchants/#{@bike_shop.id}"
+    
+    expect(page).to have_content("Average Item Price: $66.67")
+  end 
 end
