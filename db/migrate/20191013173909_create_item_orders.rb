79540736beb242b9d1bacb5ae35f192d407734be
Scott Regenthal <scottregenthal@gmail.com>
2019-10-13 11:42:55 -0600

    Create and migrate Orders table and ItemOrders joins table
    Co-authored-by: Ryan Hantak <47759923+rhantak@users.noreply.github.com>

---
+++ db/migrate/20191013173909_create_item_orders.rb
@@ -0,0 +1,12 @@
+class CreateItemOrders < ActiveRecord::Migration[5.1]
+  def change
+    create_table :item_orders do |t|
+      t.references :item, foreign_key: true
+      t.references :order, foreign_key: true
+      t.integer :quantity
+      t.integer :subtotal
+
+      t.timestamps
+    end
+  end
+end
