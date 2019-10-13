cf51f968cae7a74bc9258c08bfa09117100cc1e6
Laura Schulz <lrs8810@gmail.com>
2019-10-13 14:52:21 -0600

    Add merchant statistics spec and functionality
    Co-Authored-By: Mary Lang <mcat56@users.noreply.github.com>

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,10 +1,25 @@
 class Merchant <ApplicationRecord
   has_many :items
-  
+
   validates_presence_of :name,
                         :address,
                         :city,
                         :state,
                         :zip
 
+  def average_item_price
+    if items.empty?
+      return
+    else
+      items.average(:price).round(2)
+    end 
+  end
+
+  def distinct_cities
+    order_ids = items.map do |item|
+      ItemOrder.where(item_id: item.id).pluck(:order_id)
+    end.flatten
+
+    Order.where(id: order_ids).pluck(:city).uniq
+  end
 end
