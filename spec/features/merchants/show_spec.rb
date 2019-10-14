789fcfccd9b452de6a492ac977a4171524d9512b
Michael Cooper <macoop2363@gmail.com>
2019-10-14 13:10:06 -0600

    Add tests for merchant statistics
    Co-authored-by: John Travers <46035439+johnktravers@users.noreply.github.com>

--- spec/features/merchants/show_spec.rb
+++ spec/features/merchants/show_spec.rb
@@ -48,4 +48,33 @@ RSpec.describe 'merchant show page', type: :feature do
     
     expect(page).to have_content("Average Item Price: $66.67")
   end 
+
+  it 'can show a list of distinct cities where their items have been ordered' do
+    @meg = Merchant.create(name: "Meg's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
+
+    @tire = @meg.items.create(name: "Gatorskins", description: "They'll never pop!", price: 100, image: "https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588", inventory: 12)
+
+    @pull_toy = @meg.items.create(name: "Pull Toy", description: "Great pull toy!", price: 10, image: "http://lovencaretoys.com/image/cache/dog/tug-toy-dog-pull-9010_2-800x800.jpg", inventory: 32)
+    @dog_bone = @meg.items.create(name: "Dog Bone", description: "They'll love it!", price: 20, image: "https://img.chewy.com/is/image/catalog/54226_MAIN._AC_SL1500_V1534449573_.jpg", active?:false, inventory: 21)
+
+    user_1 = User.create(name: 'Kyle Pine', address: '123 Main Street', city: 'Greenville', state: 'NC', zip: '29583')
+    order_1 = user_1.orders.create(grand_total: 100)
+    order_1.item_orders.create(item_id: @tire.id, item_quantity: 2, subtotal: 50)
+    
+    user_2 = User.create(name: 'Steve Spruce', address: '456 2nd Street', city: 'Redville', state: 'SC', zip: '29444')
+    order_2 = user_2.orders.create(grand_total: 40)
+    order_2.item_orders.create(item_id: @pull_toy.id, item_quantity: 2, subtotal: 20)
+    order_2.item_orders.create(item_id: @dog_bone.id, item_quantity: 1, subtotal: 20)
+
+    user_3 = User.create(name: 'Steve Spruce', address: '456 2nd Street', city: 'Redville', state: 'SC', zip: '29444')
+    order_3 = user_3.orders.create(grand_total: 40)
+    order_3.item_orders.create(item_id: @pull_toy.id, item_quantity: 2, subtotal: 20)
+    order_3.item_orders.create(item_id: @dog_bone.id, item_quantity: 1, subtotal: 20)
+
+    visit "/merchants/#{@meg.id}"
+
+    within '#statistics' do
+      expect(page).to have_content("Items sold in these cities:\nGreenville\nRedville")
+    end
+  end
 end
