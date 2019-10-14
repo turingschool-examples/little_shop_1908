22ab8bd7fbcc39d8b5c11844acb5867834ac457c
Graham Thompson <grwthomps@gmail.com>
2019-10-14 11:40:08 -0600

    Create order and item_order models, edit item model for many to many relationship
    Co-Authored-By: Christopher Kelling <cjkelling@users.noreply.github.com>

---
+++ app/models/item_order.rb
@@ -0,0 +1,4 @@
+class ItemOrder < ApplicationRecord
+  belongs_to :items
+  belongs_to :orders
+end
---
+++ app/models/order.rb
@@ -0,0 +1,4 @@
+class Order < ApplicationRecord
+  has_many :item_orders
+  has_many :items, through: :item_orders
+end
