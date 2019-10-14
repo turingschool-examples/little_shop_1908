75fc3d510f5d6e9795932b316a46ccc49f7b3d32
Jon Patterson <JonPatt92@gmail.com>
2019-10-14 16:18:07 -0600

    Refactor cart show_spec test to account for untested flash message

--- spec/features/cart/checkout_spec.rb
+++ spec/features/cart/checkout_spec.rb
@@ -1,22 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe 'Cart Show Page' do
-  # before(:each) do
-  #   @bike_shop = Merchant.create(name: "Brian's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
-  #   @meg = Merchant.create(name: "Meg's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
-  #
-  #
-  #   @chain = @bike_shop.items.create(name: "Chain", description: "It'll never break!", price: 50, image: "https://www.rei.com/media/b61d1379-ec0e-4760-9247-57ef971af0ad?size=784x588", inventory: 5)
-  #   @tire = @meg.items.create(name: "Gatorskins", description: "They'll never pop!", price: 100, image: "https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588", inventory: 12)
-  #
-  #   visit "items/#{@chain.id}"
-  #   click_on "Add to cart"
-  #
-  #   visit "items/#{@tire.id}"
-  #   click_on "Add to cart"
-  #
-  #   visit '/cart'
-  # end
 
   it 'can checkout when I have items in my cart' do
     @bike_shop = Merchant.create(name: "Brian's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
