90f4ce28d4e8ec0299875fef504f4b9935bd2cd6
Michael Cooper <macoop2363@gmail.com>
2019-10-14 13:12:10 -0600

    Add orders method to get a merchant's orders
    Co-authored-by: John Travers <46035439+johnktravers@users.noreply.github.com>

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -9,4 +9,8 @@ class Merchant < ApplicationRecord
 
   validates_numericality_of :zip, only_integer: true
   validates_length_of :zip, is: 5
+
+  def orders
+    orders = Order.where(id: ItemOrder.where(item_id: items.pluck(:id)).pluck(:order_id))
+  end
 end
