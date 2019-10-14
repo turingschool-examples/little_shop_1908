e72325bbfe6d60befcbd31515c73f8ecc78bd704
Jon Patterson <JonPatt92@gmail.com>
2019-10-14 13:34:08 -0600

    Build create method for order

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -9,7 +9,16 @@ class OrdersController < ApplicationController
   end
 
   def create
-    new_order = Order.create(order_params)
+    new_order = Order.new(order_params)
+    if new_order.save
+      cart.each do |item_id, quantity|
+        item = Item.find(item_id)
+        OrderItem.create(order_id: "#{new_order.id}", item_id: item_id, price: "#{item.price}", quantity: quantity)
+      end
+    else
+      flash.now[:notice] = "Please finish filling out form before submitting."
+      render :new
+    end
   end
 
   private
