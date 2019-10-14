9782a155bdfbcd7085ff76551328cbffadf908c7
Ryan Hantak <rhantak@gmail.com>
2019-10-14 11:23:57 -0600

    Update order model testing
    Co-authored-by: Scott Regenthal <50811220+freeheeling@users.noreply.github.com>

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -14,4 +14,31 @@ describe Order, type: :model do
     it {should have_many :item_orders}
     it {should have_many(:items).through(:item_orders)}
   end
+
+  describe '#generate_item_orders' do
+    it "can generate item_orders for every item in the order" do
+      @meg = Merchant.create(name: "Meg's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
+      @tire = @meg.items.create(name: "Gatorskins", description: "They'll never pop!", price: 100, image: "https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588", inventory: 12)
+      @chain = @meg.items.create(name: "Chain", description: "It'll never break!", price: 50, image: "https://www.rei.com/media/b61d1379-ec0e-4760-9247-57ef971af0ad?size=784x588", inventory: 5)
+
+      cart = Cart.new({})
+      cart.add_item(@tire.id)
+      cart.add_item(@tire.id)
+      cart.add_item(@chain.id)
+
+      order = Order.create(name: "Bob", address: "123 Street", city: "Denver", state: "CO", zip: "80232", grand_total: 250)
+
+      order.generate_item_orders(cart)
+
+      expect(ItemOrder.first.order_id).to eq(order.id)
+      expect(ItemOrder.first.item_id).to eq(@tire.id)
+      expect(ItemOrder.first.quantity).to eq(2)
+      expect(ItemOrder.first.subtotal).to eq(200)
+
+      expect(ItemOrder.last.order_id).to eq(order.id)
+      expect(ItemOrder.last.item_id).to eq(@chain.id)
+      expect(ItemOrder.last.quantity).to eq(1)
+      expect(ItemOrder.last.subtotal).to eq(50)
+    end
+  end
 end
--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -15,4 +15,28 @@ describe Review, type: :model do
   describe "relationships" do
     it { should belong_to :item }
   end
+
+  before(:each) do
+    @bike_shop = Merchant.create(name: "Brian's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
+    @chain = @bike_shop.items.create(name: "Chain", description: "It'll never break!", price: 50, image: "https://www.rei.com/media/b61d1379-ec0e-4760-9247-57ef971af0ad?size=784x588", inventory: 5)
+    @review_1 = @chain.reviews.create(title: "first review", content: "content", rating: 4)
+    @review_2 = @chain.reviews.create(title: "second review", content: "more content", rating: 5)
+    @review_3 = @chain.reviews.create(title: "third review", content: "more content", rating: 4)
+    @review_4 = @chain.reviews.create(title: "fourth review", content: "more content", rating: 3)
+    @review_5 = @chain.reviews.create(title: "fifth review", content: "more content", rating: 1)
+    @review_6 = @chain.reviews.create(title: "sixth review", content: "more content", rating: 2)
+    @review_7 = @chain.reviews.create(title: "seventh review", content: "more content", rating: 2)
+  end
+
+  it "can calculate the average rating of all reviews" do
+    expect(Review.avg_rating).to eq(3.00)
+  end
+
+  it "can identify the 3 lowest rated reviews" do
+    expect(Review.bottom).to eq([@review_5, @review_6, @review_7])
+  end
+
+  it "can identify the 3 highest rated reviews" do
+    expect(Review.top).to eq([@review_2, @review_3, @review_1])
+  end
 end
