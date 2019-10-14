0b0eae9b63f6268c3a5bb35f940b2bb0be28938e
Jon Patterson <JonPatt92@gmail.com>
2019-10-14 12:34:18 -0600

    Add migration for order_items table. Model tests passing

---
+++ db/migrate/20191014183248_create_order_items.rb
@@ -0,0 +1,10 @@
+class CreateOrderItems < ActiveRecord::Migration[5.1]
+  def change
+    create_table :order_items do |t|
+      t.references :item, foreign_key: true
+      t.references :order, foreign_key: true
+      t.integer :price
+      t.integer :quantity
+    end
+  end
+end
