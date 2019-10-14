b321c223f61179275fc4471bc3f5deb47b42f322
Ryan Hantak <rhantak@gmail.com>
2019-10-14 13:51:18 -0600

    Add method for merchant to check if it has item_orders
    Co-authored-by: Scott Regenthal <50811220+freeheeling@users.noreply.github.com>

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,10 +1,18 @@
 class Merchant <ApplicationRecord
   has_many :items
-  
+
   validates_presence_of :name,
                         :address,
                         :city,
                         :state,
                         :zip
 
+
+  def has_item_orders?
+    checks = items.map do |item|
+      ItemOrder.where(item_id: item.id).any?
+    end
+    return true if checks.include?(true)
+    return false
+  end
 end
