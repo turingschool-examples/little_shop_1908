efa55a751f0449e0d356051b528a1f36b65e9198
Ryan Hantak <rhantak@gmail.com>
2019-10-14 10:50:46 -0600

    Remove unused methods from item_order model
    Co-authored-by: Scott Regenthal <50811220+freeheeling@users.noreply.github.com>

--- app/models/item_order.rb
+++ app/models/item_order.rb
@@ -5,18 +5,6 @@ class ItemOrder < ApplicationRecord
   validates_presence_of :item_id,
                         :order_id
 
-  def find(item_id, order_id)
-    ItemOrder.find_by(item_id: item_id, order_id: order_id)
-  end
-
-  def self.find_all(order_id)
-    item_orders = []
-    ItemOrder.pluck(:item_id).each do |item_id|
-      item_orders << ItemOrder.find(item_id, order_id)
-    end
-    item_orders
-  end
-
   def current_item
     Item.find(item_id)
   end
--- spec/models/item_order_spec.rb
+++ spec/models/item_order_spec.rb
@@ -10,11 +10,16 @@ RSpec.describe ItemOrder, type: :model do
     it { should belong_to :order }
   end
 
-  it 'can find an item' do
-    meg = Merchant.create(name: "Meg's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
-    tire = meg.items.create(name: "Gatorskins", description: "They'll never pop!", price: 100, image: "https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588", inventory: 12)
-    order= Order.create(name: "name", address: "123 a street", city: "city", state: "co", zip: "80232", grand_total: 200)
-    item_order = ItemOrder.create( item_id: "#{tire.id}", order_id: "#{order.id}", quantity: 2, subtotal: 200)
-    expect(item_order.current_item).to eq(tire)
+  before(:each) do
+    @meg = Merchant.create(name: "Meg's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
+    @tire = @meg.items.create(name: "Gatorskins", description: "They'll never pop!", price: 100, image: "https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588", inventory: 12)
+    @chain = @meg.items.create(name: "Chain", description: "It'll never break!", price: 50, image: "https://www.rei.com/media/b61d1379-ec0e-4760-9247-57ef971af0ad?size=784x588", inventory: 5)
+    @order= Order.create(name: "name", address: "123 a street", city: "city", state: "co", zip: "80232", grand_total: 200)
+    @item_order_1 = ItemOrder.create( item_id: "#{@tire.id}", order_id: "#{@order.id}", quantity: 2, subtotal: 200)
+    @item_order_2 = ItemOrder.create( item_id: "#{@chain.id}", order_id: "#{@order.id}", quantity: 3, subtotal: 150)
+  end
+
+  it 'can find an item in the item order' do
+    expect(@item_order_1.current_item).to eq(@tire)
   end
 end
