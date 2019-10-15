1b7786c3d7c4af8740111cdef0e4a5cd2d024ea9
John Travers <johnk.travers@gmail.com>
2019-10-15 15:31:41 -0600

    Empty cart in create order action
    Co-authored-by: Michael Cooper <8922930+CoopTang@users.noreply.github.com>

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -20,6 +20,8 @@ class OrdersController < ApplicationController
       cart.contents.each do |item_id, quantity|
         order.item_orders.create(item_id: item_id, item_quantity: quantity, subtotal: cart.subtotal(item_id))
       end
+      cart.empty_contents
+      session[:cart] = Hash.new(0)
       redirect_to "/orders/#{order.id}"
     else
       flash[:error] = user.errors.full_messages
