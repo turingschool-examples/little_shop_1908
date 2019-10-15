d07a10e169bfddc3c7c045b38bde88267ae525d5
Ryan Hantak <rhantak@gmail.com>
2019-10-15 11:06:30 -0600

    Add number_of_items, avg_price, and all_cities methods to merchant model
    Co-authored-by: Scott Regenthal <50811220+freeheeling@users.noreply.github.com>

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,6 +1,7 @@
 class Merchant <ApplicationRecord
   has_many :items
   has_many :item_orders, through: :items
+  has_many :orders, through: :item_orders
 
   validates_presence_of :name,
                         :address,
@@ -11,5 +12,17 @@ class Merchant <ApplicationRecord
 
   def has_item_orders?
     !item_orders.empty?
+  end
+
+  def number_of_items
+    items.count
+  end
+
+  def avg_price
+    items.average(:price)
+  end
+
+  def all_cities
+    orders.pluck(:city).uniq
   end
 end
