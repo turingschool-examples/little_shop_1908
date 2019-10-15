7aa1f9703fc43800deaca43626af5b1afadb82ab
Graham Thompson <grwthomps@gmail.com>
2019-10-15 11:11:54 -0600

    Create has_orders? method in merchant model
    Co-Authored-By: Christopher Kelling <cjkelling@users.noreply.github.com>

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -7,6 +7,16 @@ class Merchant <ApplicationRecord
                         :state,
                         :zip
 
-validates_length_of :zip, :is => 5
-validates :zip, numericality: true
+  validates_length_of :zip, :is => 5
+  validates :zip, numericality: true
+
+  def has_orders?
+    arr = []
+    items.each do |item|
+      if item.item_orders.count > 0
+        arr << false
+      end
+    end
+    arr.include?(false)
+  end
 end
