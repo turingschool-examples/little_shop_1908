e2dcd9317f68f287367349d7b9a88f1e8bf69949
John Travers <johnk.travers@gmail.com>
2019-10-14 18:08:15 -0600

    Add reviews, users, and orders to seed file
    Co-authored-by: Michael Cooper <8922930+CoopTang@users.noreply.github.com>

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,87 +6,264 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-# Merchant.destroy_all
-# Item.destroy_all
-# Review.destroy_all
+#---------------------------- Merchants ----------------------------#
+florist = Merchant.create(
+  name: 'Florist Gump',
+  address: '1523 N Main Street',
+  city: 'Plaintree',
+  state: 'MN',
+  zip: 49155
+)
+fuschia = Merchant.create(
+  name: 'Back to the Fuschia',
+  address: '943 Burberry Drive',
+  city: 'Kirksville',
+  state: 'NC',
+  zip: 30846
+)
+pine_oakio = Merchant.create(
+  name: 'Pine Oakio',
+  address: '12 Montgomery Lane',
+  city: 'Alden',
+  state: 'AL',
+  zip: 61350
+)
 
-merchants = Merchant.create([
+#---------------------------- Items ----------------------------#
+plumeria = Item.create(
+  name: 'Plumeria Plant',
+  description: 'The plumeria plant, also known as the "Scent of Hawaii", is like no other plant. It has electric hues of yellow, pink, and white blossoms that bloom from April until November. It also has a slender, geometric shape, and soft foliage that will branch and produce up to 60 flowers and over 100 blossoms each year.',
+  price: 93.20,
+  image: 'https://images.pexels.com/photos/63609/plumeria-flower-frangipani-plant-63609.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+  active?: true,
+  inventory: 14,
+  merchant_id: florist.id
+)
+dahlia = Item.create(
+  name: 'Dahlia Bulbs - Contraste Variety',
+  description: 'A timeless dahlia favorite, introduced almost 60 years ago and still going strong. The big blooms measure up to eight inches across and have striking two-tone petals that are deep burgundy and purple with brilliant white tips.',
+  price: 15.40,
+  image: 'https://images.pexels.com/photos/910645/pexels-photo-910645.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+  active?: true,
+  inventory: 32,
+  merchant_id: florist.id
+)
+rose = Item.create(
+  name: 'Clementine Rose Bush',
+  description: 'This rose bush grows long, pointed buds that open to classically shaped, four-inch blooms that have an artistic feel to their color - a rich apricot-blush, over-layed with copper tones toward the edge of the petals. The striking blooms are plentifully produced against bright-green, glossy leaves.',
+  price: 45.63,
+  image: 'https://images.pexels.com/photos/53007/rose-rose-family-rosaceae-composites-53007.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+  active?: false,
+  inventory: 8,
+  merchant_id: florist.id
+)
+hibiscus = Item.create(
+  name: 'Pink Tropical Hibiscus Tree',
+  description: 'Adding a tropical feel to your garden or landscape has never been easier. The pink tropical hibiscus tree is a low-maintenance dwarf tree, reaching only 6-8 feet in height. Its breathtaking blooms occur year-round',
+  price: 108.65,
+  image: 'https://images.pexels.com/photos/244796/pexels-photo-244796.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+  active?: true,
+  inventory: 6,
+  merchant_id: pine_oakio.id
+)
+olive = Item.create(
+  name: 'Arbequina Olive Tree',
+  description: "It's the most versatile variety on the market: The Arbequina Olive Tree. Not only does it produce table olives, but it's also used to make highly-valued olive oil. And this tree offers evergreen beauty and fragrant spring blooms to go along with its succulent fruit.",
+  price: 84.99,
+  image: 'https://images.pexels.com/photos/1047312/pexels-photo-1047312.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+  active?: false,
+  inventory: 4,
+  merchant_id: pine_oakio.id
+)
+
+#---------------------------- Users ----------------------------#
+kyle = User.create(
+  name: 'Kyle Pine',
+  address: '763 Chesterfield Street',
+  city: 'Jacksonville',
+  state: 'FL',
+  zip: 38342
+)
+cynthia = User.create(
+  name: 'Cynthia Hall',
+  address: '9247 E 42nd Avenue',
+  city: 'Tucson',
+  state: 'AZ',
+  zip: 85701
+)
+
+#---------------------------- Orders ----------------------------#
+order_1 = kyle.orders.create(grand_total: rose.price + dahlia.price * 2)
+order_1.item_orders.create(item_id: dahlia.id, item_quantity: 2, subtotal: dahlia.price * 2)
+order_1.item_orders.create(item_id: rose.id, item_quantity: 1, subtotal: rose.price)
+
+order_2 = cynthia.orders.create(grand_total: plumeria.price + rose.price * 3 + hibiscus.price)
+order_2.item_orders.create(item_id: plumeria.id, item_quantity: 1, subtotal: plumeria.price)
+order_2.item_orders.create(item_id: rose.id, item_quantity: 3, subtotal: rose.price * 3)
+order_2.item_orders.create(item_id: hibiscus.id, item_quantity: 1, subtotal: hibiscus.price)
+
+#---------------------------- Reviews ----------------------------#
+plumeria.reviews.create([
   {
-    name: 'Florist Gump',
-    address: '1523 N Main Street',
-    city: 'Plaintree',
-    state: 'MN',
-    zip: 49155
+    title: 'Beautiful!',
+    content: "The most beautiful plant I've ever grown!",
+    rating: 5
   },
   {
-    name: 'Back to the Fuschia',
-    address: '943 Burberry Drive',
-    city: 'Kirksville',
-    state: 'NC',
-    zip: 30846
+    title: 'Meh.',
+    content: 'Does not survive snowy weather.',
+    rating: 2
   },
   {
-    name: 'Pine Oakio',
-    address: '12 Montgomery Lane',
-    city: 'Alden',
-    state: 'AL',
-    zip: 61350
+    title: 'I love this plant',
+    content: 'It brings me great joy',
+    rating: 5
+  },
+  {
+    title: 'Foliage is not soft',
+    content: 'Misleading description',
+    rating: 1
+  },
+  {
+    title: 'Needs lots of water',
+    content: 'Cannot go a single day without watering',
+    rating: 3
+  },
+  {
+    title: 'Blooms for a long time!',
+    content: 'The flowers stay beautiful for weeks.',
+    rating: 4
+  },
+  {
+    title: 'Very cool plant',
+    content: 'Adds tropical flair to my home',
+    rating: 5
   }
 ])
-items = Item.create([
+
+dahlia.reviews.create([
   {
-    name: 'Plumeria Plant',
-    description: 'The plumeria plant, also known as the "Scent of Hawaii", is like no other plant. It has electric hues of yellow, pink, and white blossoms that bloom from April until November. It also has a slender, geometric shape, and soft foliage that will branch and produce up to 60 flowers and over 100 blossoms each year.',
-    price: 93.20,
-    image: 'https://images.pexels.com/photos/63609/plumeria-flower-frangipani-plant-63609.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    active?: true,
-    inventory: 14,
-    merchant_id: merchants[0].id
+    title: 'Lovely flowers',
+    content: "I can't wait for them to come back next year!",
+    rating: 4
   },
   {
-    name: 'Dahlia Bulbs - Contraste Variety',
-    description: 'A timeless dahlia favorite, introduced almost 60 years ago and still going strong. The big blooms measure up to eight inches across and have striking two-tone petals that are deep burgundy and purple with brilliant white tips.',
-    price: 15.40,
-    image: 'https://images.pexels.com/photos/910645/pexels-photo-910645.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    active?: true,
-    inventory: 32,
-    merchant_id: merchants[0].id
+    title: 'Never grew',
+    content: 'I followed the directions but they never sprouted',
+    rating: 1
   },
   {
-    name: 'Clementine Rose Bush',
-    description: 'This rose bush grows long, pointed buds that open to classically shaped, four-inch blooms that have an artistic feel to their color - a rich apricot-blush, over-layed with copper tones toward the edge of the petals. The striking blooms are plentifully produced against bright-green, glossy leaves.',
-    price: 45.63,
-    image: 'https://images.pexels.com/photos/53007/rose-rose-family-rosaceae-composites-53007.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    active?: false,
-    inventory: 8,
-    merchant_id: merchants[0].id
+    title: 'Very showy!',
+    content: 'Love showing them off to the neighbors',
+    rating: 5
   },
   {
-    name: 'Pink Tropical Hibiscus Tree',
-    description: 'Adding a tropical feel to your garden or landscape has never been easier. The pink tropical hibiscus tree is a low-maintenance dwarf tree, reaching only 6-8 feet in height. Its breathtaking blooms occur year-round',
-    price: 108.65,
-    image: 'https://images.pexels.com/photos/244796/pexels-photo-244796.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    active?: true,
-    inventory: 6,
-    merchant_id: merchants[2].id
+    title: 'Excellent coloring',
+    content: 'Wonderful contrast between petals',
+    rating: 4
   },
   {
-    name: 'Arbequina Olive Tree',
-    description: "It's the most versatile variety on the market: The Arbequina Olive Tree. Not only does it produce table olives, but it's also used to make highly-valued olive oil. And this tree offers evergreen beauty and fragrant spring blooms to go along with its succulent fruit.",
-    price: 84.99,
-    image: 'https://images.pexels.com/photos/1047312/pexels-photo-1047312.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    active?: false,
-    inventory: 4,
-    merchant_id: merchants[2].id
+    title: 'Great for beginners',
+    content: "They don't require a lot of special attential",
+    rating: 5
   },
+  {
+    title: 'Short but beautiful blooms',
+    content: 'Only bloomed for a day. Lovely flowers though',
+    rating: 3
+  }
 ])
-items[0].reviews.create(
-  title: "Beautiful!",
-  content: "The most beautiful plant I've ever grown!",
-  rating: 5
-)
-items[0].reviews.create(
-  title: "Meh!",
-  content: "Does not survive snowy weather.",
-  rating: 2
-)
+
+rose.reviews.create([
+  {
+    title: 'Most beautiful rose bush',
+    content: 'Wonderful to look at',
+    rating: 5
+  },
+  {
+    title: 'Breathtaking color',
+    content: 'Somewhere between peach and sunset orange',
+    rating: 5
+  },
+  {
+    title: 'Not enough blooms',
+    content: 'I do love the shape of the bush though',
+    rating: 4
+  },
+  {
+    title: 'Buds open slowly',
+    content: 'It keeps the rose-like feeling for longer than most rose bushes',
+    rating: 5
+  },
+  {
+    title: 'Very hardy',
+    content: 'Survived -30 degrees F and lots of snow',
+    rating: 5
+  },
+  {
+    title: 'Lots of thorns',
+    content: "Beautiful rose bush. Just don't touch it",
+    rating: 4
+  }
+])
+
+hibiscus.reviews.create([
+  {
+    title: 'Very tropical',
+    content: 'It brightens up my Minnesota garden',
+    rating: 5
+  },
+  {
+    title: "Doesn't bloom often",
+    content: 'Blooms are quite pretty though',
+    rating: 4
+  },
+  {
+    title: 'Smaller than expected',
+    content: 'Definitely would not call this a tree',
+    rating: 2
+  },
+  {
+    title: 'Dies very easily',
+    content: 'I watered it every day and it still died',
+    rating: 1
+  },
+  {
+    title: 'Magnificent color',
+    content: 'Love the bright pink',
+    rating: 5
+  },
+  {
+    title: "It's just OK",
+    content: 'Nothing too special',
+    rating: 3
+  }
+])
+
+olive.reviews.create([
+  {
+    title: 'Does not produce fruit',
+    content: 'The description is one big lie!',
+    rating: 1
+  },
+  {
+    title: "Meh. It's a tree.",
+    content: 'Not great',
+    rating: 3
+  },
+  {
+    title: 'Pot is too small',
+    content: 'Definitely needs a bigger pot',
+    rating: 2
+  },
+  {
+    title: 'Somehow has thorns?!',
+    content: 'What kind of olive tree is this?',
+    rating: 1
+  },
+  {
+    title: 'Awful shade of green',
+    content: 'Leaves are not olive green at all',
+    rating: 2
+  }
+])
