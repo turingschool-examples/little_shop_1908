ea5a13ff68bc541e38f7c04216e4e15db115a703
Christopher Kelling <cj.kelling88@gmail.com>
2019-10-16 11:34:41 -0600

    Refactor order create method to product and show order number
    Co-Authored-By: Graham Thompson <grwthomps@gmail.com>

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,14 +1,15 @@
 class OrdersController < ApplicationController
-  def new
-  end
+  def new; end
 
   def create
     order = Order.create(order_params)
+    order.order_number = ('%010d' % rand(10**10))
     if order.save
       cart.contents.each do |item, quantity|
         ItemOrder.create(item_id: item.to_i, order_id: order.id, quantity: quantity, price: Item.find(item.to_i).price.to_f)
       end
       session.delete(:cart)
+      flash[:success] = "Your order number is #{order.order_number}"
       redirect_to "/orders/#{order.id}"
     else
       flash.now[:error] = order.errors.full_messages.to_sentence
@@ -17,12 +18,22 @@ class OrdersController < ApplicationController
   end
 
   def show
-    @order = Order.find(params[:order_id])
+    if params.include?(:search)
+      order = Order.search(params[:search])
+      if order
+        @order = order
+      else
+        flash[:error] = 'Order not found'
+        redirect_to '/items'
+      end
+    else
+      @order = Order.find(params[:order_id])
+    end
   end
 
   private
 
   def order_params
-    params.permit(:customer_name, :customer_address, :customer_city, :customer_state, :customer_zip)
+    params.permit(:customer_name, :customer_address, :customer_city, :customer_state, :customer_zip, :search)
   end
 end
