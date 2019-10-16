8694952db3dd64ad31e7b2942d886b461ae3cd27
Brian Bower <brian@brianbower.com>
2019-10-16 14:51:28 -0600

    Add test and logic for merchant item count and avg price.

--- spec/features/merchants/show_spec.rb
+++ spec/features/merchants/show_spec.rb
@@ -23,5 +23,31 @@ RSpec.describe 'merchant show page', type: :feature do
       expect(current_path).to eq("/merchants/#{@bike_shop.id}/items")
     end
 
+    it "can see stats for our merchant" do
+      @meg = Merchant.create(name: "Meg's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
+      @tire = @meg.items.create(name: "Gatorskins", description: "They'll never pop!", price: 100, image: "https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588", inventory: 12)
+
+      visit "/items/#{@tire.id}"
+      click_button "Add Item"
+      visit '/cart'
+      click_link 'Checkout'
+      visit '/orders/new'
+
+      fill_in :name, with: 'Michael Jackson'
+      fill_in :address, with: '123 Neverland Ranch Rd'
+      fill_in :city, with: 'Hollywood'
+      fill_in :state, with: 'California'
+      fill_in :zip, with: '90210'
+
+      click_button "Create Order"
+
+      visit "/merchants/#{@meg.id}"
+
+      expect(page).to have_content("Item Count: 1")
+      expect(page).to have_content("Average Item Price: 100")
+      expect(page).to have_content("Cities With Order: Hollywood")
+
+    end
+
   end
 end
