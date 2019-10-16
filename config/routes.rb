8d6fce1316ab314fe836cf3f0afb7e226c9ff005
Christopher Kelling <cj.kelling88@gmail.com>
2019-10-16 13:24:12 -0600

    Edited routes for orders
    Co-Authored-By: Graham Thompson <grwthomps@gmail.com>

--- config/routes.rb
+++ config/routes.rb
@@ -1,36 +1,38 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/merchants', to: 'merchants#index'
+  get '/merchants/new', to: 'merchants#new'
+  get '/merchants/:id', to: 'merchants#show'
+  post '/merchants', to: 'merchants#create'
+  get '/merchants/:id/edit', to: 'merchants#edit'
+  patch '/merchants/:id', to: 'merchants#update'
+  delete '/merchants/:id', to: 'merchants#destroy'
+  get '/merchants/:merchant_id/items', to: 'items#index'
+  get '/merchants/:merchant_id/items/new', to: 'items#new'
+  post '/merchants/:merchant_id/items', to: 'items#create'
 
-  get "/merchants", to: "merchants#index"
-  get "/merchants/new", to: "merchants#new"
-  get "/merchants/:id", to: "merchants#show"
-  post "/merchants", to: "merchants#create"
-  get "/merchants/:id/edit", to: "merchants#edit"
-  patch "/merchants/:id", to: "merchants#update"
-  delete "/merchants/:id", to: "merchants#destroy"
-  get "/merchants/:merchant_id/items", to: "items#index"
-  get "/merchants/:merchant_id/items/new", to: "items#new"
-  post "/merchants/:merchant_id/items", to: "items#create"
-
-  get "/items", to: "items#index"
-  get "/items/:id", to: "items#show"
-  get "/items/:id/edit", to: "items#edit"
-  get "/items/:item_id/reviews/new", to: "reviews#new"
-  post "/items/:item_id", to: "reviews#create"
+  get '/items', to: 'items#index'
+  get '/items/:id', to: 'items#show'
+  get '/items/:id/edit', to: 'items#edit'
+  get '/items/:item_id/reviews/new', to: 'reviews#new'
+  post '/items/:item_id', to: 'reviews#create'
   get '/items/:item_id/reviews/:review_id/edit', to: 'reviews#edit'
   patch '/items/:item_id/reviews/:review_id', to: 'reviews#update'
   delete '/items/:item_id/reviews/:review_id', to: 'reviews#destroy'
-  patch "/items/:id", to: "items#update"
-  delete "/items/:id", to: "items#destroy"
+  patch '/items/:id', to: 'items#update'
+  delete '/items/:id', to: 'items#destroy'
 
-  get '/cart', to: "cart#show"
-  patch '/cart/:item_id', to: "cart#update"
-  patch '/cart/remove_item/:item_id', to: "cart#remove_item"
-  delete '/cart', to: "cart#destroy"
+  get '/cart', to: 'cart#show'
+  patch '/cart/:item_id', to: 'cart#update'
+  patch '/cart/remove_item/:item_id', to: 'cart#remove_item'
+  delete '/cart', to: 'cart#destroy'
   patch '/cart/:add_subtract/:item_id', to: 'cart#add_subtract'
 
-  get '/checkout', to: "orders#new"
-  post "/orders/:order_id", to: "orders#create"
-  get "/orders/:order_id", to: "orders#show"
-  get "/verified_order", to: "orders#verified_order"
+  get '/checkout', to: 'orders#new'
+  post '/orders/:order_id', to: 'orders#create'
+  get '/orders/:order_id', to: 'orders#show'
+  get '/orders', to: 'orders#index'
+  get '/verified_order', to: 'orders#verified_order'
+  delete '/orders/:order_id', to: 'orders#destroy'
+
+  delete '/item_orders/:item_order_id', to: 'item_orders#destroy'
 end
