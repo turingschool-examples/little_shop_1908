ab57aa3ac8875ecde8a60b68bc7c295183c9555b
John Travers <johnk.travers@gmail.com>
2019-10-16 16:16:46 -0600

    Add test for removing items from orders on verified order page
    Co-authored-by: Michael Cooper <8922930+CoopTang@users.noreply.github.com>

--- spec/features/orders/show_spec.rb
+++ spec/features/orders/show_spec.rb
@@ -102,10 +102,12 @@ RSpec.describe 'order show page', type: :feature do
     before :each do
       @meg = Merchant.create(name: "Meg's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
       @tire = @meg.items.create(name: "Gatorskins", description: "They'll never pop!", price: 100, image: "https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588", inventory: 12)
+      @pull_toy = @meg.items.create(name: "Pull Toy", description: "Great pull toy!", price: 10, image: "http://lovencaretoys.com/image/cache/dog/tug-toy-dog-pull-9010_2-800x800.jpg", inventory: 32)
 
       @user_1 = User.create(name: 'Kyle Pine', address: '123 Main Street', city: 'Greenville', state: 'NC', zip: '29583')
-      @order_1 = @user_1.orders.create(grand_total: 100, verification_code: '4856752493')
+      @order_1 = @user_1.orders.create(grand_total: 120, verification_code: '4856752493')
       @order_1.item_orders.create(item_id: @tire.id, item_quantity: 2, subtotal: 50)
+      @order_1.item_orders.create(item_id: @pull_toy.id, item_quantity: 2, subtotal: 20)
     end
 
     it 'can search for an order and is taken to verified order page if found' do
@@ -151,6 +153,39 @@ RSpec.describe 'order show page', type: :feature do
 
       expect(current_path).to eq("/orders/#{@order_1.id}")
       expect(page).to have_content("Joe Bob 1331 17th Ave Denver, CO 80202")
+    end
+
+    it 'can remove items from an order and redirect to order show page' do
+      visit '/'
+      fill_in 'Order Code/Number', with: '4856752493'
+      click_button 'Search'
+
+      within "#item-#{@tire.id}" do
+        click_button 'Remove Item'
+      end
+
+      expect(current_path).to eq("/orders/#{@order_1.id}")
+      expect(page).to_not have_css("#item-#{@tire.id}")
+      expect(@order_1.item_orders.find_by(item_id: @tire.id)).to eq(nil)
+    end
+
+    it 'removing final item redirects to cart' do
+      visit '/'
+      fill_in 'Order Code/Number', with: '4856752493'
+      click_button 'Search'
+
+      within "#item-#{@tire.id}" do
+        click_button 'Remove Item'
+      end
+
+      fill_in 'Order Code/Number', with: '4856752493'
+      click_button 'Search'
+
+      within "#item-#{@pull_toy.id}" do
+        click_button 'Remove Item'
+      end
+
+      expect(current_path).to eq('/cart')
     end
   end
 end
