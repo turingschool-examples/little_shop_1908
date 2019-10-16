8b0f337c4d3f05773fb069524789b614e6db8f79
John Travers <johnk.travers@gmail.com>
2019-10-15 22:04:26 -0600

    Add tests for top rated items appearing on merchant show page

--- spec/features/merchants/show_spec.rb
+++ spec/features/merchants/show_spec.rb
@@ -5,6 +5,11 @@ RSpec.describe 'merchant show page', type: :feature do
   before :each do
     @bike_shop = Merchant.create(name: "Brian's Bike Shop", address: '123 Bike Rd.', city: 'Richmond', state: 'VA', zip: '23137')
     @chain = @bike_shop.items.create(name: "Chain", description: "It'll never break!", price: 50, image: "https://www.rei.com/media/b61d1379-ec0e-4760-9247-57ef971af0ad?size=784x588", inventory: 5)
+
+    @meg = Merchant.create(name: "Meg's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
+    @tire = @meg.items.create(name: "Gatorskins", description: "They'll never pop!", price: 100, image: "https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588", inventory: 12)
+    @pull_toy = @meg.items.create(name: "Pull Toy", description: "Great pull toy!", price: 10, image: "http://lovencaretoys.com/image/cache/dog/tug-toy-dog-pull-9010_2-800x800.jpg", inventory: 32)
+    @dog_bone = @meg.items.create(name: "Dog Bone", description: "They'll love it!", price: 20, image: "https://img.chewy.com/is/image/catalog/54226_MAIN._AC_SL1500_V1534449573_.jpg", active?:false, inventory: 21)
   end
 
   it 'can see a merchants name, address, city, state, zip' do
@@ -33,7 +38,7 @@ RSpec.describe 'merchant show page', type: :feature do
 
     expect(page).to have_content("Total Items: 1")
 
-    chain = @bike_shop.items.create(name: "Chain", description: "It'll never break!", price: 50, image: "https://www.rei.com/media/b61d1379-ec0e-4760-9247-57ef971af0ad?size=784x588", inventory: 5)
+    @bike_shop.items.create(name: "Chain", description: "It'll never break!", price: 50, image: "https://www.rei.com/media/b61d1379-ec0e-4760-9247-57ef971af0ad?size=784x588", inventory: 5)
 
     visit "/merchants/#{@bike_shop.id}"
 
@@ -43,8 +48,8 @@ RSpec.describe 'merchant show page', type: :feature do
   end
 
   it 'can see the average price of all their items' do
-    chain = @bike_shop.items.create(name: "Chain", description: "It'll never break!", price: 50, image: "https://www.rei.com/media/b61d1379-ec0e-4760-9247-57ef971af0ad?size=784x588", inventory: 5)
-    @tire = @bike_shop.items.create(name: "Gatorskins", description: "They'll never pop!", price: 100, image: "https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588", inventory: 12)
+    @bike_shop.items.create(name: "Chain", description: "It'll never break!", price: 50, image: "https://www.rei.com/media/b61d1379-ec0e-4760-9247-57ef971af0ad?size=784x588", inventory: 5)
+    @bike_shop.items.create(name: "Gatorskins", description: "They'll never pop!", price: 100, image: "https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588", inventory: 12)
 
     visit "/merchants/#{@bike_shop.id}"
 
@@ -54,13 +59,6 @@ RSpec.describe 'merchant show page', type: :feature do
   end
 
   it 'can show a list of distinct cities where their items have been ordered' do
-    @meg = Merchant.create(name: "Meg's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
-
-    @tire = @meg.items.create(name: "Gatorskins", description: "They'll never pop!", price: 100, image: "https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588", inventory: 12)
-
-    @pull_toy = @meg.items.create(name: "Pull Toy", description: "Great pull toy!", price: 10, image: "http://lovencaretoys.com/image/cache/dog/tug-toy-dog-pull-9010_2-800x800.jpg", inventory: 32)
-    @dog_bone = @meg.items.create(name: "Dog Bone", description: "They'll love it!", price: 20, image: "https://img.chewy.com/is/image/catalog/54226_MAIN._AC_SL1500_V1534449573_.jpg", active?:false, inventory: 21)
-
     user_1 = User.create(name: 'Kyle Pine', address: '123 Main Street', city: 'Greenville', state: 'NC', zip: '29583')
     order_1 = user_1.orders.create(grand_total: 100)
     order_1.item_orders.create(item_id: @tire.id, item_quantity: 2, subtotal: 50)
@@ -90,5 +88,37 @@ RSpec.describe 'merchant show page', type: :feature do
     within '#statistics' do
       expect(page).to have_content('No items have been sold 🥺')
     end
+  end
+
+  it 'shows the merchants top three items by average review rating' do
+    @tire.reviews.create(title: 'Great tire.', content: 'Really fills up.', rating: 4)
+    @tire.reviews.create(title: 'Awesome', content: 'Love this tire!', rating: 5)
+    @pull_toy.reviews.create(title: 'Less than ideal', content: 'Popped', rating: 2)
+    @dog_bone.reviews.create(title: 'Not quite round', content: 'Really close though', rating: 3)
+
+    visit "/merchants/#{@meg.id}"
+
+    expect(page).to have_content('Top Rated Items')
+
+    expect(page.find_all('.top-three-items')[0]).to have_content(@tire.name)
+    expect(page.find_all('.top-three-items')[0]).to have_css("img[src='#{@tire.image}']")
+    expect(page.find_all('.top-three-items')[0]).to have_content('Average Review Rating: 4.5')
+
+    expect(page.find_all('.top-three-items')[1]).to have_content(@dog_bone.name)
+    expect(page.find_all('.top-three-items')[1]).to have_css("img[src='#{@dog_bone.image}']")
+    expect(page.find_all('.top-three-items')[1]).to have_content('Average Review Rating: 3')
+
+    expect(page.find_all('.top-three-items')[2]).to have_content(@pull_toy.name)
+    expect(page.find_all('.top-three-items')[2]).to have_css("img[src='#{@pull_toy.image}']")
+    expect(page.find_all('.top-three-items')[2]).to have_content('Average Review Rating: 2')
+  end
+
+  it 'does not show top rated items if a merchant has no items' do
+    @meg.items.destroy_all
+
+    visit "/merchants/#{@meg.id}"
+
+    expect(page).to_not have_content('Top Rated Items')
+    expect(page).to_not have_css('.top-three-items')
   end
 end
