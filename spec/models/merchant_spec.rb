97283aa1728b7770097caa91153f1e6c41b02b65
Christopher Kelling <cj.kelling88@gmail.com>
2019-10-16 12:21:27 -0600

    Updated model tests to reach 100% coverage
    Co-Authored-By: Graham Thompson <grwthomps@gmail.com>

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -18,6 +18,8 @@ describe Merchant, type: :model do
       @bike_shop = Merchant.create!(name: "Brian's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80_203)
       @chain = @bike_shop.items.create!(name: 'Chain', description: "It'll never break!", price: 50, image: 'https://www.rei.com/media/b61d1379-ec0e-4760-9247-57ef971af0ad?size=784x588', inventory: 5)
       @tire = @bike_shop.items.create!(name: 'Tire', description: 'This is a tire.', price: 30, image: 'https://www.rei.com/media/b61d1379-ec0e-4760-9247-57ef971af0ad?size=784x588', inventory: 2)
+      @lock = @bike_shop.items.create!(name: 'Lock', description: 'This is a lock.', price: 15, image: 'https://www.rei.com/media/b61d1379-ec0e-4760-9247-57ef971af0ad?size=784x588', inventory: 5)
+      @helmet = @bike_shop.items.create!(name: 'Helmet', description: 'This is a helmet.', price: 35, image: 'https://www.rei.com/media/b61d1379-ec0e-4760-9247-57ef971af0ad?size=784x588', inventory: 5)
       @order = Order.create!(customer_name: 'Joe Schmo', customer_address: '123 Random Dr', customer_city: 'Denver', customer_state: 'CO', customer_zip: 80_128)
       @item_order = ItemOrder.create!(item_id: @chain.id, order_id: @order.id, price: 50.00, quantity: 1)
     end
@@ -30,19 +32,30 @@ describe Merchant, type: :model do
 
     describe 'count_of_items' do
       it 'shows the count of merchants items' do
-        expect(@bike_shop.count_of_items).to eq(2)
+        expect(@bike_shop.count_of_items).to eq(4)
       end
     end
 
     describe 'average_price' do
       it 'shows the average price of items for the merchant' do
-        expect(@bike_shop.average_price).to eq(40)
+        expect(@bike_shop.average_price).to eq(32.5)
       end
     end
 
     describe 'distinct_cities' do
       it 'shows the distinct cities ordered from for the merchant' do
         expect(@bike_shop.distinct_cities.to_sentence).to eq('Denver')
+      end
+    end
+
+    describe 'Top 3 Rated Items' do
+      it 'shows the merchants top 3 rated items' do
+        review_1 = @chain.reviews.create(title: "Awesome", content: "Really really awesome", rating: 5)
+        review_2 = @tire.reviews.create(title: "Not Great", content: "Stinks a lot", rating: 1)
+        review_3 = @lock.reviews.create(title: "Mediocre", content: "What can I say? Gets the job done I guess.", rating: 3)
+        review_4 = @helmet.reviews.create(title: "Mediocre", content: "What can I say? Gets the job done I guess.", rating: 4)
+
+        expect(@bike_shop.top_items).to eq('Chain, Helmet, and Lock')
       end
     end
   end
--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -20,9 +20,20 @@ RSpec.describe Order, type: :model do
         bike_shop = Merchant.create!(name: "Brian's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80_203)
         chain = bike_shop.items.create!(name: 'Chain', description: "It'll never break!", price: 50, image: 'https://www.rei.com/media/b61d1379-ec0e-4760-9247-57ef971af0ad?size=784x588', inventory: 5)
         order = Order.create!(customer_name: 'Joe Schmo', customer_address: '123 Random Dr', customer_city: 'Denver', customer_state: 'CO', customer_zip: 80_128)
-        item_order = ItemOrder.create!(item_id: chain.id, order_id: order.id, price: 50.00, quantity: 1)
+        ItemOrder.create!(item_id: chain.id, order_id: order.id, price: 50.00, quantity: 1)
 
         expect(order.grand_total).to eq(50)
+      end
+    end
+
+    describe 'seach' do
+      it 'seaches for orders by order number' do
+        bike_shop = Merchant.create!(name: "Brian's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80_203)
+        chain = bike_shop.items.create!(name: 'Chain', description: "It'll never break!", price: 50, image: 'https://www.rei.com/media/b61d1379-ec0e-4760-9247-57ef971af0ad?size=784x588', inventory: 5)
+        order = Order.create!(customer_name: 'Joe Schmo', customer_address: '123 Random Dr', customer_city: 'Denver', customer_state: 'CO', customer_zip: 80_128, order_number: 1234567890)
+        ItemOrder.create!(item_id: chain.id, order_id: order.id, price: 50.00, quantity: 1)
+
+        expect(Order.search(1234567890)).to eq(order)
       end
     end
   end
