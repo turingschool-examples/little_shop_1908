17037edf657dd75ca0fb157198e5281363d03192
John Travers <johnk.travers@gmail.com>
2019-10-16 12:13:02 -0600

    Add validation statements for verification_code
    Co-authored-by: Michael Cooper <8922930+CoopTang@users.noreply.github.com>

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,8 +1,13 @@
 class Order < ApplicationRecord
   validates_presence_of :grand_total
+  validates_presence_of :verification_code
 
   validates_numericality_of :grand_total
   validates_numericality_of :grand_total, greater_than: 0
+
+  validates_numericality_of :verification_code, only_integer: true
+  validates_length_of :verification_code, is: 10
+  validates :verification_code, uniqueness: true
 
   has_many :item_orders
   has_many :items, through: :item_orders
