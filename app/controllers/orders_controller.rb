e9b42b57c57f91bc9ef607392ec7d297545ae24c
John Travers <johnk.travers@gmail.com>
2019-10-16 12:15:32 -0600

    Add verification code creation to create action
    Co-authored-by: Michael Cooper <8922930+CoopTang@users.noreply.github.com>

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -13,16 +13,9 @@ class OrdersController < ApplicationController
   end
 
   def create
-    user = User.create(user_params)
+    user = User.new(user_params)
     if user.save
-      order = user.orders.create(grand_total: cart.grand_total)
-
-      cart.contents.each do |item_id, quantity|
-        order.item_orders.create(item_id: item_id, item_quantity: quantity, subtotal: cart.subtotal(item_id))
-      end
-      cart.empty_contents
-      session[:cart] = Hash.new(0)
-      redirect_to "/orders/#{order.id}"
+      create_order(user)
     else
       flash[:error] = user.errors.full_messages
       redirect_to '/orders/new'
@@ -32,5 +25,22 @@ class OrdersController < ApplicationController
   private
     def user_params
       params.permit(:name, :address, :city, :state, :zip)
+    end
+
+    def create_order(user)
+      loop do
+        rand_code = rand(10000000000).to_s
+        order = user.orders.create(grand_total: cart.grand_total, verification_code: rand_code)
+        if order.save
+          cart.contents.each do |item_id, quantity|
+            order.item_orders.create(item_id: item_id, item_quantity: quantity, subtotal: cart.subtotal(item_id))
+          end
+          cart.empty_contents
+          session[:cart] = Hash.new(0)
+          flash[:success] = ["Order Verification Code: #{rand_code}"]
+          redirect_to "/orders/#{order.id}"
+          break
+        end
+      end
     end
 end
